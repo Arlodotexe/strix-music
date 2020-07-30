f7801181cde0bed7fce5bb4ffc90e6f388c91890
Avishai Dernis <avid29@live.com>
2020-07-30 10:24:12 -0400

    #50 Shell layout with Windows Toolkit

---
+++ src/UI/Strix-Music.Shared/Strix-Styles/Shell/Drawer.xaml.cs
@@ -0,0 +1,71 @@
+﻿// <copyright file="Drawer.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.Shared.Strix_Style.Shell
+{
+    /// <summary>
+    /// A Drawer used as the root of the 
+    /// </summary>
+    public sealed partial class Drawer : UserControl
+    {
+        private bool isPanelOpen = false;
+
+        public Drawer()
+        {
+            this.InitializeComponent();
+        }
+
+        public object TopBarContent
+        {
+            get { return this.topBarContent.Content; }
+            set { this.topBarContent.Content = value; }
+        }
+
+        public object PanelContent
+        {
+            get { return this.panelContent.Content; }
+            set { this.panelContent.Content = value; }
+        }
+
+        public object BottomBarContent
+        {
+            get { return this.bottomBarContent.Content; }
+            set { this.bottomBarContent.Content = value; }
+        }
+
+        public object BottomBarSecondaryContent
+        {
+            get { return this.bottomBarSecondaryContent.Content; }
+            set { this.bottomBarSecondaryContent.Content = value; }
+        }
+
+        public object MainContent
+        {
+            get { return this.mainContent.Content; }
+            set { this.mainContent.Content = value; }
+        }
+
+        private void VisualStateGroup_CurrentStateChanged(object sender, VisualStateChangedEventArgs e)
+        {
+            if (e.NewState == this.Mid)
+            {
+                this.SetPanel(false);
+            }
+        }
+
+        private void TogglePanel(object sender, RoutedEventArgs args)
+        {
+            this.SetPanel(!this.isPanelOpen);
+        }
+
+        private void SetPanel(bool open)
+        {
+            VisualStateManager.GoToState(this, open ? "Closed" : "Opened", true);
+            this.isPanelOpen = open;
+        }
+    }
+}
---
+++ src/UI/Strix-Music.Shared/Strix-Styles/Shell/Shell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Strix_Music.Shared.Strix_Style.Shell
+{
+    public sealed partial class Shell : UserControl
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
