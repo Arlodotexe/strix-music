4581b835d4fbb2e992406266349f0062a1a7a768
Matthew <40522069+matthew4850@users.noreply.github.com>
2020-07-30 23:26:45 +0100

    #50 Shell control generic template

--- DefaultShell/Controls/ShellControl.cs
+++
@@ -1,8 +0,0 @@
-﻿using Windows.UI.Xaml.Controls;
-
-namespace Strix_Music.Default.Controls
-{
-    public sealed partial class ShellControl : Control
-    {
-    }
-}
---
+++ src/UI/Strix-Music.DefaultShell/Controls/ShellControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class ShellControl : Control
+    {
+        public ShellControl()
+        {
+            this.DefaultStyleKey = typeof(ShellControl);
+        }
+    }
+}
