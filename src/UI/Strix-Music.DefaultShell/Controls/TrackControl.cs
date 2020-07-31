b352f36739cbf5d1acc653f5835d9a83a972f699
Avishai Dernis <avid29@live.com>
2020-07-30 22:28:23 -0400

    #74 Created Track control

---
+++ src/UI/Strix-Music.DefaultShell/Controls/TrackControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class TrackControl : Control
+    {
+        public TrackControl()
+        {
+            this.DefaultStyleKey = typeof(TrackControl);
+        }
+    }
+}
