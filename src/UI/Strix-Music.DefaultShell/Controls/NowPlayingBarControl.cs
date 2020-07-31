9b380184d5c1bae77062e987424b2134a86ff585
Avishai Dernis <avid29@live.com>
2020-07-30 21:18:20 -0400

    #63 Created NowPlayingBarControl

---
+++ src/UI/Strix-Music.DefaultShell/Controls/NowPlayingBarControl.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class NowPlayingBarControl : Control
+    {
+        public NowPlayingBarControl()
+        {
+            this.DefaultStyleKey = typeof(NowPlayingBarControl);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+    }
+}
