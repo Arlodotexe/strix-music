5f86f971a9315b8277edd927e628a6a6cad34ca8
Adam Dernis <avid29@live.com>
2020-07-31 01:20:21 -0400

    #82  Created Artist control

---
+++ src/UI/Strix-Music.DefaultShell/Controls/ArtistControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class ArtistControl : Control
+    {
+        public ArtistControl()
+        {
+            this.DefaultStyleKey = typeof(ArtistControl);
+        }
+    }
+}
