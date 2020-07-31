7c22f15f2a42642c426cab4a75fdc5b5e7c3cf11
Avishai Dernis <avid29@live.com>
2020-07-30 23:00:20 -0400

    #76 Created ArtistView control

---
+++ src/UI/Strix-Music.DefaultShell/Controls/ArtistViewControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class ArtistViewControl : Control
+    {
+        public ArtistViewControl()
+        {
+            this.DefaultStyleKey = typeof(ArtistViewControl);
+        }
+    }
+}
