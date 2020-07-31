01d0319e85c855cbcd53cd31d64b1ecf92054b71
Avishai Dernis <avid29@live.com>
2020-07-31 00:23:05 -0400

    #86 Created PlayableCollection control

---
+++ src/UI/Strix-Music.DefaultShell/Controls/PlayableCollectionListControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class PlayableCollectionListControl : Control
+    {
+        public PlayableCollectionListControl()
+        {
+            this.DefaultStyleKey = typeof(PlayableCollectionListControl);
+        }
+    }
+}
