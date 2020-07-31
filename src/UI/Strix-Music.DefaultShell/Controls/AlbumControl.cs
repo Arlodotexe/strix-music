f90fc946abd7723cf41cda70988127f17c5e5688
Adam Dernis <avid29@live.com>
2020-07-31 01:15:17 -0400

    #75 Created Album control

---
+++ src/UI/Strix-Music.DefaultShell/Controls/AlbumControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class AlbumControl : Control
+    {
+        public AlbumControl()
+        {
+            this.DefaultStyleKey = typeof(AlbumControl);
+        }
+    }
+}
