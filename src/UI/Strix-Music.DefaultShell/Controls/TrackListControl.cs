8291582b11c175439aa12308e9b981f96e4c8a5b
Avishai Dernis <avid29@live.com>
2020-07-30 22:17:23 -0400

    #83 TrackList control

---
+++ src/UI/Strix-Music.DefaultShell/Controls/TrackListControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class TrackListControl : Control
+    {
+        public TrackListControl()
+        {
+            this.DefaultStyleKey = typeof(TrackListControl);
+        }
+    }
+}
