b0f8cd322d4583ce8584dcb364e370f525ad42fd
Adam Dernis <avid29@live.com>
2020-07-31 18:34:11 -0400

    #64 Created Home control

---
+++ src/UI/Strix-Music.DefaultShell/Controls/HomeControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.DefaultShell.Controls
+{
+    public sealed partial class HomeControl : Control
+    {
+        public HomeControl()
+        {
+            this.DefaultStyleKey = typeof(HomeControl);
+        }
+    }
+}
