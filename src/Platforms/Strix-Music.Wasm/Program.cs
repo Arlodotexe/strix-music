fa7ebaa0622c60b754f5c214a944043874972cec
Matthew <40522069+matthew4850@users.noreply.github.com>
2020-07-31 17:51:56 +0100

    Fix android styling.

--- src/Platforms/Strix-Music.Wasm/Program.cs
+++ src/Platforms/Strix-Music.Wasm/Program.cs
@@ -5,7 +5,7 @@ namespace Strix_Music.Wasm
 {
     public class Program
     {
-        private static App _app;
+        private static App? _app;
 
         static int Main(string[] args)
         {
