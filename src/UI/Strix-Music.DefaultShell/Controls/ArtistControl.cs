5a97e042f31b6e26081b3d376650700b66d25f68
Adam Dernis <avid29@live.com>
2020-08-01 01:22:35 -0400

    #82 Added PointerOver VisualState to Artist control

--- src/UI/Strix-Music.DefaultShell/Controls/ArtistControl.cs
+++ src/UI/Strix-Music.DefaultShell/Controls/ArtistControl.cs
@@ -1,12 +1,43 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
+#pragma warning disable CS8601 // Possible null reference assignment.
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
 namespace Strix_Music.DefaultShell.Controls
 {
+    [TemplatePart(Name = nameof(rootGrid), Type = typeof(Grid))]
     public sealed partial class ArtistControl : Control
     {
+        private Grid rootGrid;
+
         public ArtistControl()
         {
             this.DefaultStyleKey = typeof(ArtistControl);
         }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            // Find and set RootGrid
+            rootGrid = GetTemplateChild(nameof(rootGrid)) as Grid;
+
+            rootGrid.PointerEntered += RootGrid_PointerEntered;
+            rootGrid.PointerExited += RootGrid_PointerExited;
+        }
+
+        private void RootGrid_PointerExited(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Normal", true);
+        }
+
+        private void RootGrid_PointerEntered(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "PointerOver", true);
+        }
     }
 }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+#pragma warning restore CS8601 // Possible null reference assignment.
