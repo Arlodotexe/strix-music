2ec177456d995a28a96f2f23b59c83ee1723e454
Adam Dernis <avid29@live.com>
2020-08-03 08:23:26 -0400

    #141 Created Root JSON type "Library"

---
+++ src/Cores/StrixMusic.Core.Dummy/Deserialization/Library.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using StrixMusic.Core.Dummy.Implementations;
+
+namespace StrixMusic.Core.Dummy
+{
+    /// <summary>
+    /// The root type for the Library stored in JSON.
+    /// </summary>
+    public class Library
+    {
+        /// <summary>
+        /// The lists of tracks in the dummy core's library.
+        /// </summary>
+        [JsonProperty("tracks")]
+        public List<DummyTrack> Tracks { get; set; }
+
+        /// <summary>
+        /// The lists of artists in the dummy core's library.
+        /// </summary>
+        [JsonProperty("artists")]
+        public List<DummyArtist> Artists { get; set; }
+
+        /// <summary>
+        /// The lists of albums in the dummy core's library.
+        /// </summary>
+        [JsonProperty("album")]
+        public List<DummyAlbum> Albums { get; set; }
+    }
+}
