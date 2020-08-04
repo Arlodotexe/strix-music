db3846638c77184bdfb41bce82e02fc897ecb727
Adam Dernis <avid29@live.com>
2020-08-03 21:48:37 -0400

    #145 ShellControl ViewModel

--- src/UI/StrixMusic.Shell.Default/Controls/ShellControl.cs
+++ src/UI/StrixMusic.Shell.Default/Controls/ShellControl.cs
@@ -1,12 +1,18 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using StrixMusix.ViewModels;
+using Windows.UI.Xaml.Controls;
 
 namespace Strix_Music.Shell.Default.Controls
 {
     public sealed partial class ShellControl : Control
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShellControl"/> class.
+        /// </summary>
         public ShellControl()
         {
             this.DefaultStyleKey = typeof(ShellControl);
         }
+
+        private MainViewModel? ViewModel => DataContext as MainViewModel;
     }
 }
