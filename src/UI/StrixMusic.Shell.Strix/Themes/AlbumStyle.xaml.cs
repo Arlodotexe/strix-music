f6435c1bd8d8a31cc3c539eb49dff03eadbcfba8
Adam Dernis <avid29@live.com>
2020-08-05 16:52:56 -0400

    #151 StrixShell AlbumStyle x:binding

---
+++ src/UI/StrixMusic.Shell.Strix/Themes/AlbumStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace Strix_Music.Shell.Strix.Themes
+{
+    public sealed partial class AlbumStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlbumStyle"/> class.
+        /// </summary>
+        public AlbumStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
