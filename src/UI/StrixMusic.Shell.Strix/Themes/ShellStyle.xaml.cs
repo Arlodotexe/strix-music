54d1580b22578e770424d9e51c5904a06eb94f66
Adam Dernis <avid29@live.com>
2020-08-04 23:44:07 -0400

    #149 StrixShell x:binding (note: TrackStyle fallback broken)

---
+++ src/UI/StrixMusic.Shell.Strix/Themes/ShellStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace Strix_Music.Shell.Strix.Themes
+{
+    public sealed partial class ShellStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShellStyle"/> class.
+        /// </summary>
+        public ShellStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
