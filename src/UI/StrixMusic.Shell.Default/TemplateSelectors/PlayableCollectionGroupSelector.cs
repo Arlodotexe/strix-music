4765d6a6fed5d4f06b14114e187cb14188915777
Adam Dernis <avid29@live.com>
2020-08-05 14:40:09 -0400

    Album LibraryTab x:Binding

---
+++ src/UI/StrixMusic.Shell.Default/TemplateSelectors/PlayableCollectionGroupSelector.cs
@@ -0,0 +1,36 @@
+﻿using StrixMusic.CoreInterfaces.Interfaces;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Strix_Music.Shell.Default.TemplateSelectors
+{
+    /// <summary>
+    /// A <see cref="DataTemplateSelector"/> for the collections in a <see cref="IPlayableCollectionGroup"/> library.
+    /// </summary>
+    public class PlayableCollectionGroupSelector : DataTemplateSelector
+    {
+        /// <summary>
+        /// The <see cref="DataTemplate"/> for track lists.
+        /// </summary>
+        public DataTemplate? AlbumTemplate { get; set; }
+
+        /// <summary>
+        /// The <see cref="DataTemplate"/> for any <see cref="IPlayableCollectionGroup"/>.
+        /// </summary>
+        public DataTemplate? ArtistTemplate { get; set; }
+
+        /// <inheritdoc/>
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            switch (item)
+            {
+                case IAlbum _:
+                    return AlbumTemplate!;
+                case IArtist _:
+                    return ArtistTemplate!;
+                default:
+                    return ArtistTemplate!;
+            }
+        }
+    }
+}
