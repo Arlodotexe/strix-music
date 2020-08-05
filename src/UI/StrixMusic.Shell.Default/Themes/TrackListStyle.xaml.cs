152b96f908fa80580f6e59e8f8b0019237c1aa94
Adam Dernis <avid29@live.com>
2020-08-04 22:30:32 -0400

    #149 TrackListControl binding

---
+++ src/UI/StrixMusic.Shell.Default/Themes/TrackListStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace Strix_Music.Shell.Default.Themes
+{
+    public sealed partial class TrackListStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackListStyle"/> class.
+        /// </summary>
+        public TrackListStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
