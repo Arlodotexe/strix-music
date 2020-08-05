454719b49abb50a0544583e4b636342b6d118a4e
Adam Dernis <avid29@live.com>
2020-08-05 19:32:11 -0400

    #153 AlbumView x:binding

---
+++ src/UI/StrixMusic.Shell.Default/Themes/AlbumViewStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace Strix_Music.Shell.Default.Themes
+{
+    public sealed partial class AlbumViewStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlbumStyle"/> class.
+        /// </summary>
+        public AlbumViewStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
