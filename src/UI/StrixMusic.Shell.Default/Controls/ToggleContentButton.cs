464612c6085cffa8b92448febcf00df1ef274527
Adam Dernis <avid29@live.com>
2020-08-07 16:57:09 -0400

    Indeterminate ToggleContentButton and Repeat button on Strix

--- src/UI/StrixMusic.Shell.Default/Controls/ToggleContentButton.cs
+++ src/UI/StrixMusic.Shell.Default/Controls/ToggleContentButton.cs
@@ -18,6 +18,15 @@ namespace StrixMusic.Shell.Default.Controls
             new PropertyMetadata(null));
 
         /// <summary>
+        /// A <see cref="DependencyProperty"/> for the <see cref="IndeterminateContent"/> property.
+        /// </summary>
+        public static readonly DependencyProperty IndeterminateContentProperty = DependencyProperty.Register(
+            nameof(IndeterminateContent),
+            typeof(object),
+            typeof(ToggleContentButton),
+            new PropertyMetadata(null));
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="ToggleContentButton"/> class.
         /// </summary>
         public ToggleContentButton()
@@ -26,12 +35,21 @@ namespace StrixMusic.Shell.Default.Controls
         }
 
         /// <summary>
-        /// The <see cref="object"/> the <see cref="ToggleContentButton"/> shows when toggled.
+        /// The <see cref="object"/> the <see cref="ToggleContentButton"/> shows when checked.
         /// </summary>
         public object CheckedContent
         {
             get { return (object)GetValue(CheckContentProperty); }
             set { SetValue(CheckContentProperty, value); }
+        }
+
+        /// <summary>
+        /// The <see cref="object"/> the <see cref="ToggleContentButton"/> shows when indeterminate.
+        /// </summary>
+        public object IndeterminateContent
+        {
+            get { return (object)GetValue(IndeterminateContentProperty); }
+            set { SetValue(IndeterminateContentProperty, value); }
         }
     }
 }
