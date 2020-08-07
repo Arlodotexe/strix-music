a5ae472c6503a7ae6f4a2f5ae5d35670a2e260c4
Adam Dernis <avid29@live.com>
2020-08-07 13:18:44 -0400

    ToggleContentButton with Zune Style

---
+++ src/UI/StrixMusic.Shell.Default/Controls/ToggleContentButton.cs
@@ -0,0 +1,37 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace StrixMusic.Shell.Default.Controls
+{
+    /// <summary>
+    /// A <see cref="ToggleButton"/> that can change content when toggled
+    /// </summary>
+    public partial class ToggleContentButton : ToggleButton
+    {
+        /// <summary>
+        /// A <see cref="DependencyProperty"/> for the <see cref="CheckedContent"/> property.
+        /// </summary>
+        public static readonly DependencyProperty CheckContentProperty = DependencyProperty.Register(
+            nameof(CheckedContent),
+            typeof(object),
+            typeof(ToggleContentButton),
+            new PropertyMetadata(null));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToggleContentButton"/> class.
+        /// </summary>
+        public ToggleContentButton()
+        {
+            this.DefaultStyleKey = typeof(ToggleContentButton);
+        }
+
+        /// <summary>
+        /// The <see cref="object"/> the <see cref="ToggleContentButton"/> shows when toggled.
+        /// </summary>
+        public object CheckedContent
+        {
+            get { return (object)GetValue(CheckContentProperty); }
+            set { SetValue(CheckContentProperty, value); }
+        }
+    }
+}
