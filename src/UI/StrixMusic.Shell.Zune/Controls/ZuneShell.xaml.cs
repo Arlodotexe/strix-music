e0b3e32739c2b0c28765c286d092d3610336c67e
Adam Dernis <avid29@live.com>
2020-08-08 16:40:02 -0400

    Zune theme swapping and UWP exclusivity

---
+++ src/UI/StrixMusic.Shell.Zune/Controls/ZuneShell.xaml.cs
@@ -0,0 +1,24 @@
+﻿using StrixMusic.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace StrixMusic.Shell.Zune.Controls
+{
+    public sealed partial class ZuneShell : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ZuneShell"/> class.
+        /// </summary>
+        public ZuneShell()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RootControl.RequestedTheme = Pivot.SelectedIndex == 0 ? ElementTheme.Dark : ElementTheme.Light;
+        }
+
+        private MainViewModel? ViewModel => DataContext as MainViewModel;
+    }
+}
