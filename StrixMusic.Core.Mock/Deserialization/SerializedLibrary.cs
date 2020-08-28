f1862107e3f6a485bf3a8265bfd3602c513949a9
Amaid.NetDev <octalogics@gmail.com>
2020-08-29 01:29:59 +0500

    WIP - mock project #171 #172

---
+++ StrixMusic.Core.Mock/Deserialization/SerializedLibrary.cs
@@ -0,0 +1,135 @@
+﻿using Newtonsoft.Json;
+using StrixMusic.Core.Mock.Implementations;
+using StrixMusic.CoreInterfaces;
+using StrixMusic.CoreInterfaces.Enums;
+using StrixMusic.CoreInterfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrixMusic.Core.Mock.Deserialization
+{
+    public class SerializedLibrary : IPlayableCollectionBase
+    {
+        /// <summary>
+        /// The lists of tracks in the dummy core's library.
+        /// </summary>
+        [JsonProperty("tracks")]
+        public List<MockTrack>? Tracks { get; set; }
+
+        /// <summary>
+        /// The lists of albums in the dummy core's library.
+        /// </summary>
+        [JsonProperty("albums")]
+        public List<MockAlbum>? Albums { get; set; }
+
+        /// <summary>
+        /// The lists of artists in the dummy core's library.
+        /// </summary>
+        [JsonProperty("artists")]
+        public List<MockArtist>? Artists { get; set; }
+
+        /// <inheritdoc/>
+        public string Id => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public ICore SourceCore => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public string Name => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public IReadOnlyList<IImage> Images => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public Uri Url => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public string? Description => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public IUserProfile? Owner => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public PlaybackState PlaybackState => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public ITrack? PlayingTrack => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public TimeSpan Duration => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public event EventHandler<PlaybackState>? PlaybackStateChanged;
+
+        /// <inheritdoc/>
+        public event EventHandler<CollectionChangedEventArgs<IImage>>? ImagesChanged;
+
+        /// <inheritdoc/>
+        public event EventHandler<string>? NameChanged
+        {
+            add
+            {
+                NameChanged += value;
+            }
+
+            remove
+            {
+                NameChanged -= value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler<string?> DescriptionChanged
+        {
+            add
+            {
+                NameChanged += value;
+            }
+
+            remove
+            {
+                NameChanged -= value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler<Uri?> UrlChanged
+        {
+            add
+            {
+                UrlChanged += value;
+            }
+
+            remove
+            {
+                UrlChanged -= value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task PauseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task PlayAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StrixMusic.Core.Mock/Implementations/MockArtist.cs
@@ -0,0 +1,75 @@
+﻿using StrixMusic.CoreInterfaces;
+using StrixMusic.CoreInterfaces.Enums;
+using StrixMusic.CoreInterfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrixMusic.Core.Mock.Implementations
+{
+    public class MockArtist : IArtist
+    {
+        public IReadOnlyList<IAlbum> Albums => throw new NotImplementedException();
+
+        public int TotalAlbumsCount => throw new NotImplementedException();
+
+        public IReadOnlyList<ITrack> Tracks => throw new NotImplementedException();
+
+        public int TotalTracksCount => throw new NotImplementedException();
+
+        public ICore SourceCore => throw new NotImplementedException();
+
+        public string Id => throw new NotImplementedException();
+
+        public Uri Url => throw new NotImplementedException();
+
+        public string Name => throw new NotImplementedException();
+
+        public IReadOnlyList<IImage> Images => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public PlaybackState PlaybackState => throw new NotImplementedException();
+
+        public TimeSpan Duration => throw new NotImplementedException();
+
+        public IReadOnlyList<IPlayableCollectionGroup> RelatedItems => throw new NotImplementedException();
+
+        public int TotalRelatedItemsCount => throw new NotImplementedException();
+
+        public event EventHandler<CollectionChangedEventArgs<IAlbum>> AlbumsChanged;
+        public event EventHandler<CollectionChangedEventArgs<ITrack>> TracksChanged;
+        public event EventHandler<PlaybackState> PlaybackStateChanged;
+        public event EventHandler<string> NameChanged;
+        public event EventHandler<string> DescriptionChanged;
+        public event EventHandler<Uri> UrlChanged;
+        public event EventHandler<CollectionChangedEventArgs<IImage>> ImagesChanged;
+        public event EventHandler<CollectionChangedEventArgs<IPlayableCollectionGroup>> RelatedItemsChanged;
+
+        public Task PauseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PlayAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PopulateAlbumsAsync(int limit, int offset = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PopulateRelatedItemsAsync(int limit, int offset = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PopulateTracksAsync(int limit, int offset = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
