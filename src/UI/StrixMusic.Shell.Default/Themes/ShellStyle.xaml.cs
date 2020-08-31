3c26ff319aa9ccf3dba9951338e953cf63ff7476
Adam Dernis <avid29@live.com>
2020-08-31 13:11:40 -0400

    Shell UserControl in ShellStyle

--- src/UI/StrixMusic.Shell.Default/Themes/ShellStyle.xaml.cs
+++ src/UI/StrixMusic.Shell.Default/Themes/ShellStyle.xaml.cs
@@ -13,9 +13,6 @@ namespace StrixMusic.Shell.Default.Themes
 {
     public sealed partial class ShellStyle : ResourceDictionary
     {
-        private readonly IReadOnlyDictionary<string, Type> _pagesMapping;
-        private INavigationService<Control> _navigationService;
-        private NavigationView? _navigationView;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ShellStyle"/> class.
@@ -23,45 +20,6 @@ namespace StrixMusic.Shell.Default.Themes
         public ShellStyle()
         {
             this.InitializeComponent();
-            _navigationService = Ioc.Default.GetService<INavigationService<Control>>();
-            _pagesMapping = new Dictionary<string, Type>
-            {
-                { "HomeItem", typeof(HomeControl) },
-            };
-            RegisterPages();
-
-            _navigationService.NavigationRequested += NavigationService_NavigationRequested;
-        }
-
-        private void NavigationService_NavigationRequested(object sender, Control e)
-        {
-            _navigationView!.Content = e;
-        }
-
-        private void RegisterPages()
-        {
-            _navigationService!.RegisterCommonPage(typeof(HomeControl));
-        }
-
-        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            string pageName = (args.SelectedItem as Control) !.Tag.ToString();
-            if (!_pagesMapping.ContainsKey(pageName))
-            {
-                return;
-            }
-
-            _navigationService!.NavigateTo(_pagesMapping[pageName]);
-        }
-
-        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
-        {
-            _navigationView = (sender as NavigationView) !;
-        }
-
-        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
-        {
-            _navigationService.NavigateTo(typeof(SearchViewControl), Ioc.Default.GetService<MainViewModel>());
         }
     }
 }
