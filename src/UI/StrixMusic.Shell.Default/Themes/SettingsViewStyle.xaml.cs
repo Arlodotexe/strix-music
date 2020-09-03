36f2129d8c91e2681ad02dc74b6651f83b6f4a76
Adam Dernis <avid29@live.com>
2020-09-02 23:15:10 -0400

    Added default Style for SettingsViewControl

---
+++ src/UI/StrixMusic.Shell.Default/Themes/SettingsViewStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace StrixMusic.Shell.Default.Themes
+{
+    public sealed partial class SettingsViewStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsViewStyle"/> class.
+        /// </summary>
+        public SettingsViewStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
