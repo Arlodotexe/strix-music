bcf804c1c6013b5733083d30514afd4d7d772316
Arlodotexe <arlo.godfrey@outlook.com>
2020-09-07 15:58:36 -0500

    #239 WIP data driven settings models

---
+++ src/Sdk/Interfaces/AbstractUI/AbstractMultiChoicePrefferedDisplayMode.cs
@@ -0,0 +1,19 @@
+﻿namespace StrixMusic.Sdk.Interfaces.AbstractUI
+{
+    /// <summary>
+    /// The preferred display mode for a <see cref="IAbstractDataList"/>.
+    /// </summary>
+    /// <remarks>The UI may choose not to respect this.</remarks>
+    public enum AbstractMultiChoicePrefferedDisplayMode
+    {
+        /// <summary>
+        /// Displays items in a dropdown menu.
+        /// </summary>
+        Dropdown,
+
+        /// <summary>
+        /// Displays items as multiple radio buttons.
+        /// </summary>
+        RadioButtons,
+    }
+}
