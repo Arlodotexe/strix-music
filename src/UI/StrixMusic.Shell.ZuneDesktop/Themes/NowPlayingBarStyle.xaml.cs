4cf22adc3acb45067b4d72149507c6b801898793
Adam Dernis <avid29@live.com>
2020-09-09 13:21:25 -0400

    Added AlbumWall and NowPlayingView to ZuneShell

---
+++ src/UI/StrixMusic.Shell.ZuneDesktop/Themes/NowPlayingBarStyle.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StrixMusic.Sdk.Services.Navigation;
+using StrixMusic.Shell.Default.Controls;
+using StrixMusic.Shell.Strix;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace StrixMusic.Shell.ZuneDesktop.Themes
+{
+    public sealed partial class NowPlayingBarStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsViewStyle"/> class.
+        /// </summary>
+        public NowPlayingBarStyle()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigateToNowPlaying(object sender, RoutedEventArgs e)
+        {
+            // TODO: Save settings changes
+            ZuneDesktopShellIoc.Ioc.GetService<INavigationService<Control>>()!.NavigateTo(typeof(NowPlayingViewControl));
+        }
+    }
+}
