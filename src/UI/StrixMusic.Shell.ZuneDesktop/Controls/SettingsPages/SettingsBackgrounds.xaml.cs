6d8e5f9d2ef26db3810a9a48ec3b696d3536a2ea
Adam Dernis <avid29@live.com>
2020-09-13 12:19:41 -0400

    SettingsBackground View ZuneShell

---
+++ src/UI/StrixMusic.Shell.ZuneDesktop/Controls/SettingsPages/SettingsBackgrounds.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace StrixMusic.Shell.ZuneDesktop.Controls.SettingsPages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsBackgrounds : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsBackgrounds"/> class.
+        /// </summary>
+        public SettingsBackgrounds()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
