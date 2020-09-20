8279d268820e12b10a47d07b2dc8d63d28eb853c
Amaid.NetDev <octalogics@gmail.com>
2020-09-20 20:32:27 +0500

    ContextNavigation service interface added #223

---
+++ src/Sdk/Services/Context/IContextNavigationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StrixMusic.Sdk.Interfaces;
+
+namespace StrixMusic.Sdk.Services.Context
+{
+    /// <summary>
+    /// A service that can be called, for example, by the app itself
+    /// when activated via a protocol URL, or an App Service.
+    /// The data would be processed and any loaded shell would be
+    /// told to navigate to a page with the context.
+    /// </summary>
+    public interface IContextNavigationService<T>
+    {
+        /// <summary>
+        /// The target core.
+        /// </summary>
+        ICore TargetCore { get; }
+
+        /// <summary>
+        /// Id of the context.
+        /// </summary>
+        string? Id { get; }
+
+        /// <summary>
+        ///  Context Uri.
+        /// </summary>
+        Uri? Uri { get; }
+
+        /// <summary>
+        /// An event that is triggered whenever navigation is requested.
+        /// </summary>
+        event EventHandler<ContextNavigateEventArgs<T>> NavigationRequested;
+    }
+}
