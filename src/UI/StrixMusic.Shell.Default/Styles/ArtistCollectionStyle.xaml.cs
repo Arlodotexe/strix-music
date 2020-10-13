2876ca51bbd1c01cbf2b93d93fb54ec2de893dfc
Adam Dernis <avid29@live.com>
2020-10-13 13:25:29 -0400

    Added backing code for Artist related ResourceDictionaries

---
+++ src/UI/StrixMusic.Shell.Default/Styles/ArtistCollectionStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace StrixMusic.Shell.Default.Styles
+{
+    public sealed partial class ArtistCollectionStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArtistCollectionStyle"/> class.
+        /// </summary>
+        public ArtistCollectionStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
