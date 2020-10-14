f066ad62d285ca446f3b18f2a41dc2a3cfb8ffbe
Adam Dernis <avid29@live.com>
2020-10-13 21:04:07 -0400

    Improved AlbumView adaptive UI

--- src/UI/StrixMusic.Shell.Default/Converters/CollectionAnyToVisibilityConverter.cs
+++ src/UI/StrixMusic.Shell.Default/Converters/CollectionAnyToVisibilityConverter.cs
@@ -1,22 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 using System.Linq;
+using Uno.Extensions.Specialized;
 using Windows.UI.Xaml;
 
 namespace StrixMusic.Shell.Default.Converters
 {
     /// <summary>
-    /// A simple converter that converts a given <see cref="ICollection{T}"/> to an <see cref="Visibility"/> based on the presence of any items in the <see cref="ICollection{T}"/>.
+    /// A simple converter that converts a given <see cref="ICollection"/> to an <see cref="Visibility"/> based on the presence of any items in the <see cref="ICollection"/>.
     /// </summary>
     public sealed class CollectionAnyToVisibilityConverter
     {
         /// <summary>
-        /// Converts an <see cref="ICollection{T}"/> an <see cref="Visibility"/> based on the presence of any items in the <see cref="ICollection{T}"/>.
+        /// Converts an <see cref="ICollection"/> an <see cref="Visibility"/> based on the presence of any items in the <see cref="ICollection"/>.
         /// </summary>
-        /// <param name="value">The <see cref="ICollection{T}"/>.</param>
+        /// <param name="value">The <see cref="ICollection"/>.</param>
         /// <returns>A <see cref="Visibility"/>.</returns>
         public static Visibility Convert(object value)
         {
-            if (value is ICollection<object> collection)
+            if (value is ICollection collection)
             {
                 return collection.Any() ? Visibility.Visible : Visibility.Collapsed;
             }
