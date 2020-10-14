f8210e7e2054dee50dc4f1609dc736f36bf0079f
Adam Dernis <avid29@live.com>
2020-10-13 21:25:37 -0400

    Added backing code to ArtistViewStyle

---
+++ src/UI/StrixMusic.Shell.Strix/Styles/ArtistViewStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace StrixMusic.Shell.Strix.Styles
+{
+    public sealed partial class ArtistViewStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArtistStyle"/> class.
+        /// </summary>
+        public ArtistViewStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
