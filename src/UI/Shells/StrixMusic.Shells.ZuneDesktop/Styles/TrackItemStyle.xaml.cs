2bdb7827eefc32a50a2850510669806fa2b4b543
Adam Dernis <avid29@live.com>
2020-10-20 18:12:50 -0400

    Added TrackList to ZuneDesktop

---
+++ src/UI/Shells/StrixMusic.Shells.ZuneDesktop/Styles/TrackItemStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace StrixMusic.Shells.ZuneDesktop.Styles
+{
+    public sealed partial class TrackItemStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackItemStyle"/> class.
+        /// </summary>
+        public TrackItemStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
