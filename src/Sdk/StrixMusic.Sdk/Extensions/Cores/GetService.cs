82b39edf684bc14a3326a3cdd40757b4c5055e98
Arlodotexe <arlo.godfrey@outlook.com>
2020-10-27 14:24:51 -0500

    Fixed crash on startup caused by nullable services on ICoreConfig

--- src/Sdk/StrixMusic.Sdk/Extensions/Cores/GetService.cs
+++ src/Sdk/StrixMusic.Sdk/Extensions/Cores/GetService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Diagnostics;
 using StrixMusic.Sdk.Core.Data;
 
 namespace StrixMusic.Sdk.Extensions
@@ -14,13 +15,37 @@ namespace StrixMusic.Sdk.Extensions
         /// </summary>
         /// <typeparam name="T">The type of service to find.</typeparam>
         /// <param name="sourceCore">The core instance to get the service from.</param>
-        /// <returns>The service, if found.</returns>
-        public static T GetService<T>(this ICore sourceCore)
+        /// <returns>The service, if found, otherwise null.</returns>
+        public static T? GetServiceSafe<T>(this ICore sourceCore)
+            where T : class
         {
             if (sourceCore == null)
                 throw new ArgumentNullException(nameof(sourceCore));
 
-            return sourceCore.CoreConfig.Services.GetService<T>();
+            return sourceCore.CoreConfig.Services?.GetService<T>();
+        }
+
+        /// <summary>
+        /// A syntactically sweet extension method to get a service from an instance of a core.
+        /// </summary>
+        /// <typeparam name="T">The type of service to find.</typeparam>
+        /// <param name="sourceCore">The core instance to get the service from.</param>
+        /// <returns>The service, if found.</returns>
+        /// <exception cref="InvalidOperationException"/>
+        public static T GetService<T>(this ICore sourceCore)
+            where T : class
+        {
+            if (sourceCore == null)
+                throw new ArgumentNullException(nameof(sourceCore));
+
+            Guard.IsNotNull(sourceCore.CoreConfig.Services, nameof(sourceCore.CoreConfig.Services));
+
+            var service = sourceCore.CoreConfig.Services.GetService<T>();
+
+            if (service is null)
+                throw new InvalidOperationException();
+
+            return service;
         }
     }
 }
