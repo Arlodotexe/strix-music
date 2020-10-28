180c1a85ed649e5ca50f83198c9d01140d89101b
Adam Dernis <avid29@live.com>
2020-10-28 03:42:48 -0400

    Added backing code to Default NowPlayingBarStyle

---
+++ src/Sdk/StrixMusic.Sdk.Uno/Styles/NowPlayingBarStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace StrixMusic.Sdk.Uno.Styles
+{
+    public sealed partial class NowPlayingBarStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NowPlayingBarStyle"/> class.
+        /// </summary>
+        public NowPlayingBarStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
