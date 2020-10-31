a824c187857ae3e9deeb094b30cba5d928a7d915
Adam Dernis <avid29@live.com>
2020-10-31 00:20:36 -0400

    Added GrooveShell as DefaultShell clone

---
+++ src/UI/Shells/StrixMusic.Shells.Groove/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using StrixMusic.Sdk.Uno.Assembly;
+using StrixMusic.Shells.Groove;
+
+[assembly: Shell(
+    shellClass: typeof(GrooveShell),
+    displayName: "Groove Music")
+]
---
+++ src/UI/Shells/StrixMusic.Shells.Groove/GrooveShell.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using StrixMusic.Sdk.Services.Navigation;
+using StrixMusic.Sdk.Uno;
+using StrixMusic.Sdk.Uno.Controls;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace StrixMusic.Shells.Groove
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GrooveShell : ShellBase
+    {
+        private readonly IReadOnlyDictionary<string, Type> _pagesMapping;
+        private INavigationService<Control>? _navigationService;
+        private Stack<Control> _history = new Stack<Control>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefaultShell"/> class.
+        /// </summary>
+        public GrooveShell()
+        {
+            InitializeComponent();
+            SetupIoc();
+
+            _navigationService!.NavigationRequested += NavigationService_NavigationRequested;
+            _navigationService!.BackRequested += Shell_BackRequested;
+
+            _pagesMapping = new Dictionary<string, Type>
+            {
+                { "My Music", typeof(HomeView) },
+                { "SettingsView", typeof(SettingsView) },
+                { "Now Playing", typeof(NowPlayingView) },
+            };
+        }
+
+        /// <inheritdoc/>
+        protected override void SetupTitleBar()
+        {
+            base.SetupTitleBar();
+
+#if NETFX_CORE
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Resources["SystemAltHighColor"] as Color?;
+#endif
+
+            SystemNavigationManager currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.BackRequested += (s, e) => _navigationService!.GoBack();
+        }
+
+        private void SetupIoc()
+        {
+            DefaultShellIoc.Initialize();
+            _navigationService = DefaultShellIoc.Ioc.GetService<INavigationService<Control>>();
+            _navigationService!.RegisterCommonPage(typeof(HomeView));
+            _navigationService!.RegisterCommonPage(typeof(NowPlayingView));
+        }
+
+        private void NavigationService_NavigationRequested(object sender, NavigateEventArgs<Control> e)
+        {
+            if (!e.IsOverlay)
+            {
+                _history.Push((Control)MainContent.Content);
+                MainContent.Content = e.Page;
+            }
+            else
+            {
+                OverlayContent.Content = e.Page;
+                OverlayContent.Visibility = Visibility.Visible;
+            }
+
+            // This isn't great, but there should only be 4 items
+            Type controlType = e.Page.GetType();
+            bool containsValue = controlType == typeof(SettingsView);
+            foreach (var value in _pagesMapping.Values)
+            {
+                containsValue = containsValue || (value == controlType);
+            }
+
+            if (!containsValue)
+            {
+                NavView.SelectedItem = null;
+            }
+            else
+            {
+                _history.Clear();
+            }
+        }
+
+        private void Shell_BackRequested(object sender, EventArgs e)
+        {
+            if (OverlayContent.Visibility == Visibility.Visible)
+            {
+                OverlayContent.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            if (_history.Count > 0)
+            {
+                MainContent.Content = _history.Pop();
+            }
+        }
+
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            _navigationService!.NavigateTo(typeof(SearchView), false, args.QueryText);
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                _navigationService!.NavigateTo(_pagesMapping[nameof(SettingsView)]);
+                return;
+            }
+
+            string invokedItemString = (args.InvokedItem as string) !;
+            if (invokedItemString == null || !_pagesMapping.ContainsKey(invokedItemString))
+            {
+                return;
+            }
+
+            _navigationService!.NavigateTo(_pagesMapping[invokedItemString], invokedItemString == "Now Playing");
+        }
+    }
+}
