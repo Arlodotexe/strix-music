6d62a0bf465256e18200800f726e1324903ef4df
Adam Dernis <avid29@live.com>
2020-11-03 15:51:54 -0500

    Rudimentary MergedImage

---
+++ src/Sdk/StrixMusic.Sdk/Data/Merged/MergedImage.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Toolkit.Diagnostics;
+using StrixMusic.Sdk.Data.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StrixMusic.Sdk.Data.Merged
+{
+    /// <summary>
+    /// Merged multiple <see cref="ICoreImage"/> into a single <see cref="IImage"/>
+    /// </summary>
+    public class MergedImage : IImage, IMerged<ICoreImage>
+    {
+        private readonly ICoreImage _preferredSource;
+        private readonly List<ICoreImage> _sources;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MergedImage"/> class.
+        /// </summary>
+        public MergedImage(IReadOnlyList<ICoreImage> sources)
+        {
+            Guard.IsNotNull(sources, nameof(sources));
+            _sources = sources.ToList();
+
+            _preferredSource = _sources[0];
+        }
+
+        /// <inheritdoc/>
+        public Uri Uri => _preferredSource.Uri;
+
+        /// <inheritdoc/>
+        public double Height => _preferredSource.Height;
+
+        /// <inheritdoc/>
+        public double Width => _preferredSource.Width;
+
+        /// <inheritdoc/>
+        public IReadOnlyList<ICore> SourceCores => Sources.Select(x => x.SourceCore).ToList();
+
+        /// <inheritdoc/>
+        public IReadOnlyList<ICoreImage> Sources => _sources;
+
+        /// <inheritdoc/>
+        IReadOnlyList<ICoreImage> ISdkMember<ICoreImage>.Sources => Sources;
+
+        /// <inheritdoc/>
+        public void AddSource(ICoreImage itemToMerge)
+        {
+            _sources.Add(itemToMerge);
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(ICoreImage other)
+        {
+            return other?.Uri == Uri;
+        }
+    }
+}
