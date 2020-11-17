bcfdfc98eba71d1bd636cf643d8091d19d2ea717
Arlodotexe <arlo.godfrey@outlook.com>
2020-11-17 08:37:57 -0600

    Added RunInBackground extension

---
+++ src/Libs/OwlCore/Extensions/AsyncExtensions/RunInBackground.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace OwlCore.Extensions
+{
+    public static partial class AsyncExtensions
+    {
+        /// <summary>
+        /// Wraps a given task in <see cref="Task.Run(Func{Task})"/>
+        /// </summary>
+        /// <param name="task">The task to execute.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static Task RunInBackground(this Task task)
+        {
+            return Task.Run(async () => await task);
+        }
+    }
+}
