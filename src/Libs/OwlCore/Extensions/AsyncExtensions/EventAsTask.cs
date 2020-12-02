3741f7888ed700854092a2b480ad67cc1e8f0682
Arlodotexe <arlo.godfrey@outlook.com>
2020-12-02 02:03:46 -0600

    Added more OwlCore async extensions / overloads

---
+++ src/Libs/OwlCore/Extensions/AsyncExtensions/EventAsTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace OwlCore.Extensions
+{
+    public static partial class AsyncExtensions
+    {
+        /// <summary>
+        /// Waits for an event to fire.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static async Task<(object? Sender, TResult Result)> EventAsTask<TResult>(Action<EventHandler<TResult>> subscribe, Action<EventHandler<TResult>> unsubscribe, TimeSpan timeout)
+        {
+            var resultCancellationToken = new CancellationTokenSource();
+            resultCancellationToken.CancelAfter(timeout);
+
+            var completionSource = new TaskCompletionSource<(object? Sender, TResult Result)>();
+
+            void EventHandler(object sender, TResult eventArgs)
+            {
+                completionSource?.SetResult((sender, eventArgs));
+            }
+
+            subscribe(EventHandler);
+
+            var result = await completionSource.Task
+                .RunInBackground(resultCancellationToken.Token)
+                .TryOrSkip();
+
+            unsubscribe(EventHandler);
+
+            return result;
+        }
+    }
+}
--- src/Libs/OwlCore/Extensions/AsyncExtensions/RunInBackground.cs
+++ src/Libs/OwlCore/Extensions/AsyncExtensions/RunInBackground.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 // ReSharper disable once CheckNamespace
@@ -14,6 +15,28 @@ namespace OwlCore.Extensions
         public static Task RunInBackground(this Task task)
         {
             return Task.Run(async () => await task);
+        }
+
+        /// <summary>
+        /// Wraps a given task in <see cref="Task.Run(Func{Task})"/>
+        /// </summary>
+        /// <param name="task">The task to execute.</param>
+        /// <param name="cancellationToken">A cancellation token for this the returned task.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static Task RunInBackground(this Task task, CancellationToken cancellationToken)
+        {
+            return Task.Run(async () => await task, cancellationToken);
+        }
+
+        /// <summary>
+        /// Wraps a given task in <see cref="Task.Run(Func{Task})"/>
+        /// </summary>
+        /// <param name="task">The task to execute.</param>
+        /// <param name="cancellationToken">A cancellation token for this the returned task.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static Task<T> RunInBackground<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            return Task.Run(async () => await task, cancellationToken);
         }
     }
 }
