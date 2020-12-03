dd08e40e5b20f5c905175203093ea1cb74efebaa
YoshiAsk <jjask7@gmail.com>
2020-12-03 17:58:16 -0600

    Attempted to start creating AbstractUI view models #293

---
+++ src/Sdk/StrixMusic.Sdk/ViewModels/AbstractUI/AbstractBooleanViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using OwlCore.AbstractUI;
+
+namespace StrixMusic.Sdk.ViewModels.AbstractUI
+{
+    /// <summary>
+    /// Contains bindable information about an <see cref="AbstractBooleanUIElement"/>.
+    /// </summary>
+    public class AbstractBooleanViewModel : ObservableObject
+    {
+        public AbstractBooleanViewModel(string id)
+        {
+        }
+    }
+}
