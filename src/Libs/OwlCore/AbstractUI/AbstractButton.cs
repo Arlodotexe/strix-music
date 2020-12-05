869eab508b3c7a9e3c0570b6652aef114d24da2e
Arlodotexe <arlo.godfrey@outlook.com>
2020-12-05 13:51:08 -0600

    Removed AbstractExternalUri. Added AbstractButton.

---
+++ src/Libs/OwlCore/AbstractUI/AbstractButton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OwlCore.AbstractUI
+{
+    /// <summary>
+    /// Presents a link to the user.
+    /// </summary>
+    /// <remarks>This can be displayed in the UI however it wants (Button, text link, Icons, custom, etc)</remarks>
+    public class AbstractButton : AbstractUIMetadata
+    {
+        /// <summary>
+        /// The label  that is displayed in the button.
+        /// </summary>
+        public string Text { get; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AbstractButton"/>.
+        /// </summary>
+        /// <param name="id"><inheritdoc cref="AbstractUIBase.Id"/></param>
+        /// <param name="text">The label that is displayed in the button.</param>
+        /// <param name="iconCode">The (optional) icon that is displayed with the label.</param>
+        public AbstractButton(string id, string text, string? iconCode = null)
+            : base(id)
+        {
+            IconCode = iconCode;
+            Text = text;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Task Click()
+        {
+            Clicked?.Invoke(this, EventArgs.Empty);
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Event that fires when the button is clicked.
+        /// </summary>
+        public event EventHandler? Clicked;
+    }
+}
---
+++ src/Libs/OwlCore/AbstractUI/AbstractButtonPreferredDisplayMode.cs
@@ -0,0 +1,19 @@
+﻿namespace OwlCore.AbstractUI
+{
+    /// <summary>
+    /// The preferred display mode for <see cref="AbstractButton"/>.
+    /// </summary>
+    /// <remarks>The UI may choose not to respect this.</remarks>
+    public enum AbstractButtonPreferredDisplayMode
+    {
+        /// <summary>
+        /// Requests a conventional button style.
+        /// </summary>
+        Button,
+
+        /// <summary>
+        /// Requests a hyperlink format.
+        /// </summary>
+        Hyperlink
+    }
+}
