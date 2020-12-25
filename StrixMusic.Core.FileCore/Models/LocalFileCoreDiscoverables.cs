14887863f48b13ef78057e492a5876c08f9b53cb
amaidniazi <amaidhassan@hotmail.com>
2020-12-25 21:28:22 +0500

    Local file core discoverables added #347

---
+++ StrixMusic.Core.FileCore/Models/LocalFileCoreDiscoverables.cs
@@ -0,0 +1,77 @@
+﻿using StrixMusic.Sdk.Data.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrixMusic.Core.FileCore.Models
+{
+    /// <summary>
+    /// Discoverable music for the <see cref="FileCore"/>.
+    /// </summary>
+    public class LocalFileCoreDiscoverables : LocalFileCorePlayableCollectionGroupBase, ICoreDiscoverables
+    {
+        /// <inheritdoc />
+        public LocalFileCoreDiscoverables(ICore sourceCore)
+            : base(sourceCore)
+        {
+        }
+
+        /// <inheritdoc />
+        public override string Id { get; protected set; } = "discoverables";
+
+        /// <inheritdoc />
+        public override Uri? Url { get; protected set; } = null;
+
+        /// <inheritdoc />
+        public override string Name { get; protected set; } = "Discoverables";
+
+        /// <inheritdoc />
+        public override string? Description { get; protected set; } = null;
+
+        /// <inheritdoc />
+        public override int TotalChildrenCount { get; internal set; } = 0;
+
+        /// <inheritdoc />
+        public override int TotalPlaylistItemsCount { get; internal set; } = 0;
+
+        /// <inheritdoc />
+        public override int TotalTracksCount { get; internal set; } = 0;
+
+        /// <inheritdoc />
+        public override int TotalAlbumItemsCount { get; internal set; } = 0;
+
+        /// <inheritdoc />
+        public override int TotalArtistItemsCount { get; internal set; } = 0;
+
+        /// <inheritdoc />
+        public override IAsyncEnumerable<ICoreAlbumCollectionItem> GetAlbumItemsAsync(int limit, int offset)
+        {
+            return AsyncEnumerable.Empty<ICoreAlbum>();
+        }
+
+        /// <inheritdoc />
+        public override IAsyncEnumerable<ICoreArtistCollectionItem> GetArtistItemsAsync(int limit, int offset)
+        {
+            return AsyncEnumerable.Empty<ICoreArtist>();
+        }
+
+        /// <inheritdoc />
+        public override IAsyncEnumerable<ICorePlayableCollectionGroup> GetChildrenAsync(int limit, int offset = 0)
+        {
+            return AsyncEnumerable.Empty<ICorePlayableCollectionGroup>();
+        }
+
+        /// <inheritdoc />
+        public override IAsyncEnumerable<ICorePlaylistCollectionItem> GetPlaylistItemsAsync(int limit, int offset)
+        {
+            return AsyncEnumerable.Empty<ICorePlaylist>();
+        }
+
+        /// <inheritdoc />
+        public override IAsyncEnumerable<ICoreTrack> GetTracksAsync(int limit, int offset = 0)
+        {
+            return AsyncEnumerable.Empty<ICoreTrack>();
+        }
+    }
+}
