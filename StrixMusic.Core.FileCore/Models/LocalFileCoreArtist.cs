c6654eaee9509296a71bdab6c3c2a2912cf81d35
amaidniazi <amaidhassan@hotmail.com>
2020-12-25 21:26:14 +0500

    Artist model added. #344
    Related Work Items: #344, #360

---
+++ StrixMusic.Core.FileCore/Models/LocalFileCoreArtist.cs
@@ -0,0 +1,240 @@
+﻿using OwlCore.Collections;
+using OwlCore.Events;
+using StrixMusic.Sdk.Data;
+using StrixMusic.Sdk.Data.Core;
+using StrixMusic.Sdk.MediaPlayback;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StrixMusic.Core.FileCore.Models
+{
+    /// <inheritdoc/>
+    public class LocalFileCoreArtist : ICoreArtist
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalFileCoreArtist"/> class.
+        /// </summary>
+        /// <param name="sourceCore">The source core.</param>
+        public LocalFileCoreArtist(ICore sourceCore)
+        {
+            SourceCore = sourceCore;
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler<PlaybackState>? PlaybackStateChanged;
+
+        /// <inheritdoc/>
+        public event EventHandler<string>? NameChanged;
+
+        /// <inheritdoc/>
+        public event EventHandler<string?>? DescriptionChanged;
+
+        /// <inheritdoc/>
+        public event EventHandler<Uri?>? UrlChanged;
+
+        /// <inheritdoc/>
+        public event EventHandler<TimeSpan>? DurationChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<int>? ImagesCountChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<int>? AlbumItemsCountChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<int>? TrackItemsCountChanged;
+
+        /// <inheritdoc />
+        public event CollectionChangedEventHandler<ICoreImage>? ImagesChanged;
+
+        /// <inheritdoc />
+        public event CollectionChangedEventHandler<ICoreAlbumCollectionItem>? AlbumItemsChanged;
+
+        /// <inheritdoc />
+        public event CollectionChangedEventHandler<ICoreTrack>? TrackItemsChanged;
+
+        /// <inheritdoc/>
+        public string Id => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public int TotalAlbumItemsCount => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public int TotalImageCount { get; } = 0;
+
+        /// <inheritdoc/>
+        public int TotalTracksCount { get; }
+
+        /// <inheritdoc/>
+        public ICore SourceCore { get; }
+
+        /// <inheritdoc/>
+        public Uri Url => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public string Name => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public string Description => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public PlaybackState PlaybackState => PlaybackState.None;
+
+        /// <inheritdoc/>
+        public TimeSpan Duration => TimeSpan.Zero;
+
+        /// <inheritdoc/>
+        public ICorePlayableCollectionGroup? RelatedItems => null;
+
+        /// <inheritdoc/>
+        public SynchronizedObservableCollection<string>? Genres { get; }
+
+        /// <inheritdoc/>
+        public bool IsPlayAsyncSupported => false;
+
+        /// <inheritdoc/>
+        public bool IsPauseAsyncSupported => false;
+
+        /// <inheritdoc/>
+        public bool IsChangeNameAsyncSupported => false;
+
+        /// <inheritdoc/>
+        public bool IsChangeDescriptionAsyncSupported => false;
+
+        /// <inheritdoc/>
+        public bool IsChangeDurationAsyncSupported => false;
+
+        /// <inheritdoc/>
+        public Task<bool> IsAddImageSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> IsAddTrackSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> IsAddAlbumItemSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> IsAddGenreSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> IsRemoveImageSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> IsRemoveTrackSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> IsRemoveAlbumItemSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> IsRemoveGenreSupported(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc/>
+        public Task ChangeDescriptionAsync(string? description)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc/>
+        public Task ChangeDurationAsync(TimeSpan duration)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc/>
+        public Task ChangeNameAsync(string name)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc/>
+        public Task PauseAsync()
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc/>
+        public Task PlayAsync()
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc/>
+        public  IAsyncEnumerable<ICoreAlbumCollectionItem> GetAlbumItemsAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public  IAsyncEnumerable<ICoreTrack> GetTracksAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task AddTrackAsync(ICoreTrack track, int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public Task AddAlbumItemAsync(ICoreAlbumCollectionItem album, int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public Task RemoveTrackAsync(int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public Task RemoveAlbumItemAsync(int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public Task AddImageAsync(ICoreImage image, int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public Task RemoveImageAsync(int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public  IAsyncEnumerable<ICoreImage> GetImagesAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
