15321b0873292b41c5632f54b3c6a83a2bca92f2
Adam Dernis <avid29@live.com>
2021-01-04 07:12:44 -0500

    TrackCollection alternating colors and Groove padding

---
+++ src/UI/Shells/StrixMusic.Shells.Groove/Styles/TrackCollectionStyle.xaml.cs
@@ -0,0 +1,19 @@
+﻿using StrixMusic.Sdk.Uno.Controls;
+using Windows.UI.Xaml;
+
+namespace StrixMusic.Shells.Groove.Styles
+{
+    /// <summary>
+    /// A <see cref="ResourceDictionary"/> containing the default style for the <see cref="TrackCollection"/>.
+    /// </summary>
+    public sealed partial class TrackCollectionStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackCollectionStyle"/> class.
+        /// </summary>
+        public TrackCollectionStyle()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
