de81392a9e4c67bb472e9e50e44e588bbe44be46
Yoshi Askharoun <jjask7@gmail.com>
2021-01-07 23:39:31 -0600

    #384 #385 Added minimal ID3MetadataScanner

---
+++ src/Cores/StrixMusic.Core.LocalFiles/MetadataScanner/ID3MetadataScanner.cs
@@ -0,0 +1,47 @@
+﻿using StrixMusic.Core.LocalFiles.Backing.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using TagLib;
+
+namespace StrixMusic.Core.LocalFiles.MetadataScanner
+{
+    /// <summary>
+    /// Class for pulling ID3 metadata out of media files.
+    /// </summary>
+    public class ID3MetadataScanner
+    {
+		/// <summary>
+		/// Scans a media file for ID3 tags.
+		/// </summary>
+		/// <param name="filePath">The path to the file.</param>
+        public TrackMetadata? ScanTrackMetadata(File.IFileAbstraction fileAbstraction)
+        {
+			try
+			{
+				using (var tagFile = File.Create(fileAbstraction))
+				{
+					// Read the raw tags
+					var tags = tagFile.Tag;
+
+					return new TrackMetadata()
+					{
+						Description = tags.Description,
+						Title = tags.Title,
+						DiscNumber = tags.Disc,
+						Duration = tagFile.Properties.Duration,
+						Genres = new List<string>(tags.Genres),
+						TrackNumber = tags.Track,
+					};
+				}
+			}
+			catch (UnsupportedFormatException ex)
+			{
+				Debug.WriteLine("UNSUPPORTED FILE");
+				Debug.WriteLine("---------------------------------------\r\n");
+				return null;
+			}
+		}
+    }
+}
