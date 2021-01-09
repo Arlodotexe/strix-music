49be9c7c4d2a1a073563cff5499b8079386d687b
amaidniazi <amaidhassan@hotmail.com>
2021-01-09 13:33:48 +0500

    Updated ID3MetadataScanner to accept file path as string.

--- src/Cores/StrixMusic.Core.LocalFiles/MetadataScanner/ID3MetadataScanner.cs
+++ src/Cores/StrixMusic.Core.LocalFiles/MetadataScanner/ID3MetadataScanner.cs
@@ -12,32 +12,32 @@ namespace StrixMusic.Core.LocalFiles.MetadataScanner
     /// </summary>
     public class ID3MetadataScanner
     {
-		/// <summary>
-		/// Scans a media file for ID3 tags.
-		/// </summary>
-		/// <param name="filePath">The path to the file.</param>
-        public TrackMetadata? ScanTrackMetadata(File.IFileAbstraction fileAbstraction)
+        /// <summary>
+        /// Scans a media file for ID3 tags.
+        /// </summary>
+        /// <param name="filePath">The path to the file.</param>
+        /// <returns>The <see cref="TrackMetadata"/> with ID3 Tags></returns>
+        public TrackMetadata? ScanTrackMetadata(string filePath)
         {
-			try
-			{
-				using (var tagFile = File.Create(fileAbstraction))
-				{
-					// Read the raw tags
-					var tags = tagFile.Tag;
+            try
+            {
+                using var tagFile = File.Create(filePath);
 
-					return new TrackMetadata()
-					{
-						Description = tags.Description,
-						Title = tags.Title,
-						DiscNumber = tags.Disc,
-						Duration = tagFile.Properties.Duration,
-						Genres = new List<string>(tags.Genres),
-						TrackNumber = tags.Track,
-					};
-				}
-			}
-			catch (UnsupportedFormatException ex)
-			{
+                // Read the raw tags
+                var tags = tagFile.Tag;
+
+                return new TrackMetadata()
+                {
+                    Description = tags.Description,
+                    Title = tags.Title,
+                    DiscNumber = tags.Disc,
+                    Duration = tagFile.Properties.Duration,
+                    Genres = new List<string>(tags.Genres),
+                    TrackNumber = tags.Track,
+                };
+            }
+			catch (UnsupportedFormatException)
+            {
 				Debug.WriteLine("UNSUPPORTED FILE");
 				Debug.WriteLine("---------------------------------------\r\n");
 				return null;
