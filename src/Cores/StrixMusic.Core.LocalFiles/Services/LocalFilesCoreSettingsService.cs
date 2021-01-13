c7b4c6651f1b5f9ac5b5e853fd0255e92881a5f3
Arlodotexe <arlo.godfrey@outlook.com>
2021-01-13 12:51:12 -0600

    Fixed an issue where LocalFileCore settings service was using the same folder for all instances.

--- src/Cores/StrixMusic.Core.LocalFiles/Services/LocalFilesCoreSettingsService.cs
+++ src/Cores/StrixMusic.Core.LocalFiles/Services/LocalFilesCoreSettingsService.cs
@@ -8,8 +8,17 @@ namespace StrixMusic.Core.LocalFiles.Services
     /// <inheritdoc />
     internal class LocalFilesCoreSettingsService : SettingsServiceBase
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="LocalFilesCoreSettingsService"/>.
+        /// </summary>
+        /// <param name="instanceId">The ID of the current <see cref="LocalFileCore"/> instance.</param>
+        public LocalFilesCoreSettingsService(string instanceId)
+        {
+            Id = $"{nameof(LocalFileCore)}.{instanceId}";
+        }
+
         /// <inheritdoc />
-        public override string Id { get; } = nameof(LocalFilesCoreSettingsService);
+        public override string Id { get; }
 
         /// <inheritdoc/>
         public override IEnumerable<Type> SettingsKeysTypes => typeof(LocalFilesCoreSettingsKeys).IntoList();
