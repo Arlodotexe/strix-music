7f796e9853ce8feef877deba5889a38cbd2b2cb3
Adam Dernis <avid29@live.com>
2021-01-17 04:06:51 -0500

    PlaylistView in default shell

---
+++ src/Sdk/StrixMusic.Sdk.Uno/Controls/PlaylistView.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using OwlCore.Extensions;
+using StrixMusic.Sdk.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace StrixMusic.Sdk.Uno.Controls
+{
+    /// <summary>
+    /// A Templated <see cref="Control"/> for showing an <see cref="PlaylistViewModel"/> as a page.
+    /// </summary>
+    public sealed partial class PlaylistView : Control
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlaylistView"/> class.
+        /// </summary>
+        /// <param name="playlistViewModel">The Playlist in view.</param>
+        public PlaylistView(PlaylistViewModel playlistViewModel)
+        {
+            this.DefaultStyleKey = typeof(PlaylistView);
+            DataContext = playlistViewModel;
+
+            LoadAsync().FireAndForget();
+        }
+
+        /// <summary>
+        /// The <see cref="PlaylistViewModel"/> for this control.
+        /// </summary>
+        public PlaylistViewModel ViewModel => (PlaylistViewModel)DataContext;
+
+        private async Task LoadAsync()
+        {
+            if (!ViewModel.PopulateMoreTracksCommand.IsRunning)
+                await ViewModel.PopulateMoreTracksCommand.ExecuteAsync(5);
+        }
+    }
+}
