5c4dab9f7c02bf3666db50c25f4c18096c1e4c3f
Adam Dernis <avid29@live.com>
2021-02-07 15:51:10 -0500

    #485 Groove Shell Notification handling and style

---
+++ src/UI/Shells/StrixMusic.Shells.Groove/Styles/NotificationAbstractUIGroupStyle.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LaunchPad.AbstractUI.Controls;
+using Windows.UI.Xaml;
+
+namespace StrixMusic.Shells.Groove.Styles
+{
+    /// <summary>
+    /// A <see cref="ResourceDictionary"/> containing the default style for the <see cref="AbstractUIGroupPresenter"/>.
+    /// </summary>
+    public sealed partial class NotificationAbstractUIGroupStyle : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotificationAbstractUIGroupStyle"/> class.
+        /// </summary>
+        public NotificationAbstractUIGroupStyle()
+        {   
+            this.InitializeComponent();
+        }
+    }
+}
