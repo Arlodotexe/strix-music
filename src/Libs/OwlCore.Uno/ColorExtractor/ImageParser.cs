2bafc5ff4546410030b1458f022fbb85baa57a06
Arlodotexe <arlo.godfrey@outlook.com>
2021-02-14 14:53:48 -0600

    Added local file support to image parser

--- src/Libs/OwlCore.Uno/ColorExtractor/ImageParser.cs
+++ src/Libs/OwlCore.Uno/ColorExtractor/ImageParser.cs
@@ -21,17 +21,23 @@ namespace OwlCore.Uno.ColorExtractor
         public static async Task<Image<Argb32>?> GetImage(string url)
         {
             if (string.IsNullOrEmpty(url))
-            {
                 return null;
-            }
 
-            return (await Image.LoadAsync(await GetImageStreamAsync(url))).CloneAs<Argb32>();
+            var imageStreamAsync = await GetImageStreamAsync(url);
+            var image = await Image.LoadAsync(imageStreamAsync);
+
+            return image.CloneAs<Argb32>();
         }
 
 
         private static async Task<Stream> GetImageStreamAsync(string url)
         {
-            var response = await HttpWebRequest.CreateHttp(url).GetResponseAsync();
+            var uri = new Uri(url);
+
+            if (uri.IsFile)
+                return File.Open(uri.LocalPath, FileMode.Open);
+
+            var response = await WebRequest.CreateHttp(url).GetResponseAsync();
             return response.GetResponseStream();
         }
 
@@ -39,31 +45,27 @@ namespace OwlCore.Uno.ColorExtractor
         /// Gets colors out of an image.
         /// </summary>
         /// <param name="image">The image to get colors from.</param>
-        /// <param name="quality">The approxiamate amount of pixels to get/</param>
+        /// <param name="quality">The approximate amount of pixels to get/</param>
         /// <returns>An array of colors that appeared as pixels in the image.</returns>
-        public static RGBColor[] GetImageColors(
-            Image<Argb32> image,
-            int quality = 1920)
+        public static RGBColor[] GetImageColors(Image<Argb32> image, int quality = 1920)
         {
-            int nth = image.Width * image.Height / quality;
+            var nth = image.Width * image.Height / quality;
+            var pixelsPerRow = image.Width / nth;
 
-            int pixelsPerRow = image.Width / nth;
+            var colors = new RGBColor[image.Height * pixelsPerRow];
 
-            RGBColor[] colors = new RGBColor[image.Height * pixelsPerRow];
-
-
-            int pos = 0;
-            for (int row = 0; row < image.Height; row++)
+            var pos = 0;
+            for (var row = 0; row < image.Height; row++)
             {
-                Span<Argb32> rowPixels = image.GetPixelRowSpan(row);
-                for (int i = 0; i < pixelsPerRow; i++)
+                var rowPixels = image.GetPixelRowSpan(row);
+                for (var i = 0; i < pixelsPerRow; i++)
                 {
-                    float b = rowPixels[i * nth].B / 255f;
-                    float g = rowPixels[i * nth].G / 255f;
-                    float r = rowPixels[i * nth].R / 255f;
+                    var b = rowPixels[i * nth].B / 255f;
+                    var g = rowPixels[i * nth].G / 255f;
+                    var r = rowPixels[i * nth].R / 255f;
                     //float a = rowPixels[i].A / 255;
 
-                    RGBColor color = new RGBColor(r, g, b);
+                    var color = new RGBColor(r, g, b);
 
                     colors[pos] = color;
                     pos++;
