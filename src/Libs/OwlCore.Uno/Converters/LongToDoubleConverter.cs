ec1076009ecc8c77aeb439f2f722619ab2e73cda
Adam Dernis <avid29@live.com>
2021-02-14 14:55:01 -0500

    WIP Binding playback to NowPlayingBar

---
+++ src/Libs/OwlCore.Uno/Converters/LongToDoubleConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwlCore.Uno
+{
+    public partial class Converters
+    {
+        public static double LongToDouble(long input)
+        {
+            return (double)input;
+        }
+    }
+}
