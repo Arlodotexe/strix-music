ebc0ea00b3a3268539df68cd274fd68784570430
Arlodotexe <arlo.godfrey@outlook.com>
2021-02-14 13:35:31 -0600

    Fixed nullable silencer

--- src/Sdk/StrixMusic.Sdk/MainViewModel.cs
+++ src/Sdk/StrixMusic.Sdk/MainViewModel.cs
@@ -220,7 +220,7 @@ namespace StrixMusic.Sdk
         IReadOnlyList<IDevice> IAppCore.Devices => Devices;
 
         /// <inheritdoc />
-        ILibrary IAppCore.Library { get; } = null!;
+        ILibrary IAppCore.Library => Library ?? throw new InvalidOperationException($"{nameof(MainViewModel)} not yet initialized.");
 
         /// <inheritdoc />
         IPlayableCollectionGroup? IAppCore.Pins { get; }
