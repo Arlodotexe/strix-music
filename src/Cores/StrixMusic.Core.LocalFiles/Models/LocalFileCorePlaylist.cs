ecc1aeeaacc3b1c8e6e1af8ccaa6b30277ba7736
amaidniazi <amaidhassan@hotmail.com>
2021-02-21 13:43:23 +0500

    Added localFileCorePlaylist model.

---
+++ src/Cores/StrixMusic.Core.LocalFiles/Models/LocalFileCorePlaylist.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OwlCore.Collections;
+using OwlCore.Events;
+using StrixMusic.Sdk.Data;
+using StrixMusic.Sdk.Data.Core;
+using StrixMusic.Sdk.MediaPlayback;
+using StrixMusic.Sdk.Services.FileMetadataManager.Models;
+
+namespace StrixMusic.Core.LocalFiles.Models
+{
+    /// <inheritdoc />
+    public class LocalFileCorePlaylist : ICorePlaylist
+    {
+        private readonly PlaylistMetadata _playlistMetadata;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="LocalFileCorePlaylist"/>
+        /// </summary>
+        public LocalFileCorePlaylist(ICore sourceCore, PlaylistMetadata playlistMetadata)
+        {
+            SourceCore = sourceCore;
+            _playlistMetadata = playlistMetadata;
+        }
+
+        /// <inheritdoc />
+        public Task<bool> IsAddImageAvailable(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> IsRemoveImageAvailable(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task RemoveImageAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public int TotalImageCount { get; }
+
+        /// <inheritdoc />
+        public event EventHandler<int>? ImagesCountChanged;
+
+        /// <inheritdoc />
+        public string Id { get; }
+
+        /// <inheritdoc />
+        public Uri? Url { get; }
+
+        /// <inheritdoc />
+        public string Name { get; }
+
+        /// <inheritdoc />
+        public string? Description { get; }
+
+        /// <inheritdoc />
+        public DateTime? LastPlayed { get; }
+
+        /// <inheritdoc />
+        public PlaybackState PlaybackState { get; }
+
+        /// <inheritdoc />
+        public TimeSpan Duration { get; }
+
+        /// <inheritdoc />
+        public bool IsChangeNameAsyncAvailable { get; }
+
+        /// <inheritdoc />
+        public bool IsChangeDescriptionAsyncAvailable { get; }
+
+        /// <inheritdoc />
+        public bool IsChangeDurationAsyncAvailable { get; }
+
+        /// <inheritdoc />
+        public Task ChangeNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task ChangeDescriptionAsync(string? description)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task ChangeDurationAsync(TimeSpan duration)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public event EventHandler<PlaybackState>? PlaybackStateChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<string>? NameChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<string?>? DescriptionChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<Uri?>? UrlChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<TimeSpan>? DurationChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<DateTime?>? LastPlayedChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<bool>? IsChangeNameAsyncAvailableChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<bool>? IsChangeDescriptionAsyncAvailableChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<bool>? IsChangeDurationAsyncAvailableChanged;
+
+        /// <inheritdoc />
+        public DateTime? AddedAt { get; }
+
+        /// <inheritdoc />
+        public int TotalTracksCount { get; }
+
+        /// <inheritdoc />
+        public bool IsPlayTrackCollectionAsyncAvailable => false;
+
+        /// <inheritdoc />
+        public bool IsPauseTrackCollectionAsyncAvailable => false;
+
+        /// <inheritdoc />
+        public Task PlayTrackCollectionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task PauseTrackCollectionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task RemoveTrackAsync(int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> IsAddTrackAvailable(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> IsRemoveTrackAvailable(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public event EventHandler<bool>? IsPlayTrackCollectionAsyncAvailableChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<bool>? IsPauseTrackCollectionAsyncAvailableChanged;
+
+        /// <inheritdoc />
+        public event EventHandler<int>? TrackItemsCountChanged;
+
+        /// <inheritdoc />
+        public SynchronizedObservableCollection<string>? Genres { get; }
+
+        /// <inheritdoc />
+        public Task<bool> IsAddGenreAvailable(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc />
+        public Task<bool> IsRemoveGenreAvailable(int index)
+        {
+            return Task.FromResult(false);
+        }
+
+        /// <inheritdoc />
+        public ICore SourceCore { get; }
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<ICoreImage> GetImagesAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <inheritdoc />
+        public Task AddImageAsync(ICoreImage image, int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public event CollectionChangedEventHandler<ICoreImage>? ImagesChanged;
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<ICoreTrack> GetTracksAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task AddTrackAsync(ICoreTrack track, int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public Task PlayTrackCollectionAsync(ICoreTrack track)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc />
+        public event CollectionChangedEventHandler<ICoreTrack>? TrackItemsChanged;
+
+        /// <inheritdoc />
+        public ICoreUserProfile? Owner { get; }
+
+        /// <inheritdoc />
+        public ICorePlayableCollectionGroup? RelatedItems { get; }
+    }
+}
