5e689a12e88dc1061de6f5981d986b0307cb6ede
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-05-02 17:24:42 +0000

    Created CachedHttpClientHandler class

---
+++ src/Libs/OwlCore/Net/HttpClientHandlers/CachedHttpClientHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwlCore.Net.HttpClientHandlers
+{
+    /// <summary>
+    /// An <see cref="HttpClientHandler"/> that provides rate limiting functionality.
+    /// </summary>
+    public class CachedHttpClientHandler : HttpClientHandler
+    {
+        private readonly CachedHttpClientHandlerAction _rateLimiterAction;
+
+        /// <summary>
+        /// Creates an instance of the <see cref="CachedHttpClientHandlerAction"/>.
+        /// </summary>
+        public CachedHttpClientHandler(string cacheFolderPath, TimeSpan defaultCacheTime)
+        {
+            _rateLimiterAction = new CachedHttpClientHandlerAction(cacheFolderPath, defaultCacheTime);
+        }
+
+        /// <summary>
+        /// Creates an instance of the <see cref="CachedHttpClientHandlerAction"/>.
+        /// </summary>
+        public CachedHttpClientHandler(string cacheFolderPath, TimeSpan defaultCacheTime, CacheRequestFilter cacheRequestFilter)
+        {
+            _rateLimiterAction = new CachedHttpClientHandlerAction(cacheFolderPath, defaultCacheTime, CacheRequestFilter cacheRequestFilter);
+        }
+
+        /// <inheritdoc />
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _rateLimiterAction.SendAsync(request, cancellationToken, () => base.SendAsync(request, cancellationToken));
+        }
+    }
+}
