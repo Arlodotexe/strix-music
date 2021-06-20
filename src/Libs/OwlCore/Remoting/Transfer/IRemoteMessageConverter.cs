6690b050c375269644b738c519a38421704ca916
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-06-20 23:31:08 +0000

    Add missing docs to IRemoteMessageConverter

--- src/Libs/OwlCore/Remoting/Transfer/IRemoteMessageConverter.cs
+++ src/Libs/OwlCore/Remoting/Transfer/IRemoteMessageConverter.cs
@@ -1,10 +1,18 @@
-﻿using System.Threading.Tasks;
+﻿using OwlCore.Remoting.Transfer.MessageConverters;
+using System.Threading.Tasks;
 
 namespace OwlCore.Remoting.Transfer
 {
     /// <summary>
     /// Prepares the data in a <see cref="IRemoteMemberMessage"/> for generic data transfer.
     /// </summary>
+    /// <remarks>
+    /// Several default implementations are given in the <see cref="MessageConverters"/> namespace.
+    /// Of these, the <see cref="NewtonsoftRemoteMessageConverter"/> is the most reliable and should be used by default, unless you know what you're doing.
+    /// <para>
+    /// When crafting your own <see cref="IRemoteMessageConverter"/>, keep in mind that unless you're extra careful to remote only primitive types, object instances being serialized may need to handle circular references.
+    /// </para>
+    /// </remarks>
     public interface IRemoteMessageConverter
     {
         /// <summary>
@@ -17,7 +25,7 @@ namespace OwlCore.Remoting.Transfer
         /// <summary>
         /// Converts the given byte array back to a valid <see cref="IRemoteMemberMessage"/>.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="bytes">The byte array to deserialize.</param>
         /// <returns></returns>
         public Task<IRemoteMemberMessage> DeserializeAsync(byte[] bytes);
     }
