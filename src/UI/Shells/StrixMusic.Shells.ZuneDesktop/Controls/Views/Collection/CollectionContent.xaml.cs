f5bcd8737b6598f2a449f552d0cb0e491c736d86
Adam Dernis <avid29@live.com>
2021-06-25 19:47:51 -0400

    Added page transition animation for Collection page

--- src/UI/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/CollectionContent.xaml.cs
+++ src/UI/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/CollectionContent.xaml.cs
@@ -3,6 +3,7 @@ using StrixMusic.Sdk.Uno.Controls.Collections.Events;
 using StrixMusic.Sdk.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 
 namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collections
 {
@@ -28,6 +29,25 @@ namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collections
         private void ArtistsPageSelected(object sender, RoutedEventArgs e)
         {
             VisualStateManager.GoToState(this, "Artists", true);
+            PageTransition.Begin();
+        }
+
+        private void AlbumsPageSelected(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Albums", true);
+            PageTransition.Begin();
+        }
+
+        private void SongsPageSelected(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Songs", true);
+            PageTransition.Begin();
+        }
+
+        private void PlaylistPageSelected(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Playlists", true);
+            PageTransition.Begin();
         }
 
         private void ArtistSelected(object sender, SelectionChangedEventArgs<ArtistViewModel> e)
@@ -49,21 +69,6 @@ namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collections
         {
             e.SelectedItem?.PopulateMoreTracksCommand.Execute(20);
             TrackCollection.DataContext = e.SelectedItem;
-        }
-
-        private void AlbumsPageSelected(object sender, RoutedEventArgs e)
-        {
-            VisualStateManager.GoToState(this, "Albums", true);
-        }
-
-        private void SongsPageSelected(object sender, RoutedEventArgs e)
-        {
-            VisualStateManager.GoToState(this, "Songs", true);
-        }
-
-        private void PlaylistPageSelected(object sender, RoutedEventArgs e)
-        {
-            VisualStateManager.GoToState(this, "Playlists", true);
         }
     }
 }
