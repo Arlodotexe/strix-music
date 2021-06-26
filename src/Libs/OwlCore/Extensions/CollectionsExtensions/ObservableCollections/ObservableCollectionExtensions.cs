c0df9ba1c3f12386042563e70699cdde0daa9356
amaidniazi <amaidhassan@hotmail.com>
2021-06-26 19:18:23 +0100

    namespace warning suppressed for ObservableCollectionExtensions

--- src/Libs/OwlCore/Extensions/CollectionsExtensions/ObservableCollections/ObservableCollectionExtensions.cs
+++ src/Libs/OwlCore/Extensions/CollectionsExtensions/ObservableCollections/ObservableCollectionExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 
+// ReSharper disable once CheckNamespace
 namespace OwlCore.Extensions
 {
     /// <summary>
