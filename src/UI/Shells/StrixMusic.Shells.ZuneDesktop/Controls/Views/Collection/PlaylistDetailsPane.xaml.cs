317f41e613b1ce26e7d8c658e059ab4ad8403a13
Adam Dernis <avid29@live.com>
2021-06-25 20:27:25 -0400

    Created PlaylistDetailsPane

---
+++ src/UI/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/PlaylistDetailsPane.xaml.cs
@@ -0,0 +1,21 @@
+﻿using StrixMusic.Sdk.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collection
+{
+    /// <summary>
+    /// A control that shows the properties of a Playlist.
+    /// </summary>
+    public sealed partial class PlaylistDetailsPane : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlaylistDetailsPane"/> class.
+        /// </summary>
+        public PlaylistDetailsPane()
+        {
+            this.InitializeComponent();
+        }
+
+        private PlaylistViewModel? ViewModel => DataContext as PlaylistViewModel;
+    }
+}
