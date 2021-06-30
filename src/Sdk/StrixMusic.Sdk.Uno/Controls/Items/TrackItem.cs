52e278d96e38991475ac75e2d6743b75edefff29
Adam Dernis <avid29@live.com>
2021-06-29 23:39:11 -0400

    Added Foreground color change when PlaybackState changes

--- src/Sdk/StrixMusic.Sdk.Uno/Controls/Items/TrackItem.cs
+++ src/Sdk/StrixMusic.Sdk.Uno/Controls/Items/TrackItem.cs
@@ -1,5 +1,7 @@
-﻿using StrixMusic.Sdk.Uno.Controls.Items.Abstract;
+﻿using StrixMusic.Sdk.MediaPlayback;
+using StrixMusic.Sdk.Uno.Controls.Items.Abstract;
 using StrixMusic.Sdk.ViewModels;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace StrixMusic.Sdk.Uno.Controls.Items
@@ -15,6 +17,36 @@ namespace StrixMusic.Sdk.Uno.Controls.Items
         public TrackItem()
         {
             this.DefaultStyleKey = typeof(TrackItem);
+
+            this.Loaded += TrackItem_Loaded;
+        }
+
+        private void TrackItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= TrackItem_Loaded;
+            AttachHandlers();
+        }
+
+        private void TrackItem_Unloaded(object sender, RoutedEventArgs e)
+        {
+            DetachHandlers();
+        }
+
+        private void AttachHandlers()
+        {
+            this.Unloaded += TrackItem_Unloaded;
+            ViewModel.PlaybackStateChanged += ViewModel_PlaybackStateChanged;
+        }
+
+        private void ViewModel_PlaybackStateChanged(object sender, PlaybackState e)
+        {
+            PlaybackState = e;
+        }
+
+        private void DetachHandlers()
+        {
+            this.Unloaded -= TrackItem_Unloaded;
+            ViewModel.PlaybackStateChanged -= ViewModel_PlaybackStateChanged;
         }
 
         /// <summary>
