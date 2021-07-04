cd3f18b435401bc4b09705e6437ba56aaa51b98d
Louis Salkeld <louissalkeld@outlook.com>
2021-07-04 23:37:14 +0100

    Move ImageSize enum into its own file

--- src/Sdk/StrixMusic.Sdk/Services/FileMetadataManager/Models/ImageMetadata.cs
+++ src/Sdk/StrixMusic.Sdk/Services/FileMetadataManager/Models/ImageMetadata.cs
@@ -9,36 +9,5 @@
         /// The unique identifier for this image.
         /// </summary>
         public string? Id { get; set; }
-
-        /// <summary>
-        /// Defines standard image sizes used by the application.
-        /// </summary>
-        public enum ImageSize
-        {
-            /// <summary>
-            /// A 64x64 image.
-            /// </summary>
-            Tiny,
-
-            /// <summary>
-            /// A 128x128 image.
-            /// </summary>
-            Small,
-
-            /// <summary>
-            /// A 256x256 image.
-            /// </summary>
-            Medium,
-
-            /// <summary>
-            /// A 512x512 image.
-            /// </summary>
-            Large,
-
-            /// <summary>
-            /// A 1024x1024 image.
-            /// </summary>
-            ExtraLarge
-        }
     }
 }
---
+++ src/Sdk/StrixMusic.Sdk/Services/FileMetadataManager/Models/ImageSize.cs
@@ -0,0 +1,33 @@
+﻿namespace StrixMusic.Sdk.Services.FileMetadataManager.Models
+{
+    /// <summary>
+    /// Defines standard image sizes used by the application.
+    /// </summary>
+    public enum ImageSize
+    {
+        /// <summary>
+        /// A 64x64 image.
+        /// </summary>
+        Tiny,
+
+        /// <summary>
+        /// A 128x128 image.
+        /// </summary>
+        Small,
+
+        /// <summary>
+        /// A 256x256 image.
+        /// </summary>
+        Medium,
+
+        /// <summary>
+        /// A 512x512 image.
+        /// </summary>
+        Large,
+
+        /// <summary>
+        /// A 1024x1024 image.
+        /// </summary>
+        ExtraLarge
+    }
+}
