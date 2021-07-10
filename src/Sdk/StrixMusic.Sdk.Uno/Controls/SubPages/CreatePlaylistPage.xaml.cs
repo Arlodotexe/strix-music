522ac1525e2fb740d288098e7496bbbf4f53e79a
Adam Dernis <avid29@live.com>
2021-07-10 02:50:16 -0400

    Localized Header for CreatePlaylist page

--- src/Sdk/StrixMusic.Sdk.Uno/Controls/SubPages/CreatePlaylistPage.xaml.cs
+++ src/Sdk/StrixMusic.Sdk.Uno/Controls/SubPages/CreatePlaylistPage.xaml.cs
@@ -1,19 +1,26 @@
-﻿using StrixMusic.Sdk.Uno.Controls.SubPages.Types;
+﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using StrixMusic.Sdk.Uno.Controls.SubPages.Types;
+using StrixMusic.Sdk.Uno.Helpers;
+using StrixMusic.Sdk.Uno.Services.Localization;
 using Windows.UI.Xaml.Controls;
 
 namespace StrixMusic.Sdk.Uno.Controls.SubPages
 {
     public sealed partial class CreatePlaylistPage : UserControl, ISubPage
     {
+        private LocalizationResourceLoader _resourceLoader;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="CreatePlaylistPage"/> class.
         /// </summary>
         public CreatePlaylistPage()
         {
             this.InitializeComponent();
+
+            _resourceLoader = Ioc.Default.GetRequiredService<LocalizationResourceLoader>();
         }
 
         /// <inheritdoc/>
-        public string Header => "Create Playlist";
+        public string Header => _resourceLoader[Constants.Localization.MusicResource, "CreatePlaylist"];
     }
 }
