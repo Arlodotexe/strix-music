82c81b3492fbec62e075459d9f0f61746e939c28
amaidniazi <amaidhassan@hotmail.com>
2021-08-07 10:44:31 +0100

    implemented CollectionInit for playlist/artist/album collection

--- src/Sdk/StrixMusic.Sdk/ViewModels/Helpers/CollectionInit.cs
+++ src/Sdk/StrixMusic.Sdk/ViewModels/Helpers/CollectionInit.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
@@ -31,6 +32,84 @@ namespace StrixMusic.Sdk.ViewModels.Helpers
                     Guard.IsAssignableToType(track, typeof(MergedTrack), nameof(track));
 
                     trackCollection.Tracks.Add(new TrackViewModel(track));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initialize a album collection view model.
+        /// </summary>
+        /// <param name="albumCollection">The collection to initialize.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static async Task AlbumCollection(IAlbumCollectionViewModel albumCollection)
+        {
+            if (albumCollection.Albums.Count < albumCollection.TotalAlbumItemsCount)
+            {
+                var albumCollectionList = await APIs.GetAllItemsAsync<IAlbumCollectionItem>(albumCollection.TotalAlbumItemsCount, async offset => await albumCollection.GetAlbumItemsAsync(albumCollection.TotalAlbumItemsCount, offset));
+
+                foreach (var item in albumCollectionList)
+                {
+                    switch (item)
+                    {
+                        case IAlbum album:
+                            albumCollection.Albums.Add(new AlbumViewModel(album));
+                            break;
+                        case IAlbumCollection collection:
+                            albumCollection.Albums.Add(new AlbumCollectionViewModel(collection));
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initialize a artist collection view model.
+        /// </summary>
+        /// <param name="artistCollection">The collection to initialize.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static async Task ArtistCollection(IArtistCollectionViewModel artistCollection)
+        {
+            if (artistCollection.Artists.Count < artistCollection.TotalArtistItemsCount)
+            {
+                var albumCollectionList = await APIs.GetAllItemsAsync<IArtistCollectionItem>(artistCollection.TotalArtistItemsCount, async offset => await artistCollection.GetArtistItemsAsync(artistCollection.TotalArtistItemsCount, offset));
+
+                foreach (var item in albumCollectionList)
+                {
+                    switch (item)
+                    {
+                        case IArtist artist:
+                            artistCollection.Artists.Add(new ArtistViewModel(artist));
+                            break;
+                        case IArtistCollection collection:
+                            artistCollection.Artists.Add(new ArtistCollectionViewModel(collection));
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initialize a playlists collection view model.
+        /// </summary>
+        /// <param name="playlistCollection">The collection to initialize.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static async Task PlaylistsCollection(IPlaylistCollectionViewModel playlistCollection)
+        {
+            if (playlistCollection.Playlists.Count < playlistCollection.TotalPlaylistItemsCount)
+            {
+                var playlistCollectionList = await APIs.GetAllItemsAsync<IPlaylistCollectionItem>(playlistCollection.TotalPlaylistItemsCount, async offset => await playlistCollection.GetPlaylistItemsAsync(playlistCollection.TotalPlaylistItemsCount, offset));
+
+                foreach (var item in playlistCollectionList)
+                {
+                    switch (item)
+                    {
+                        case IPlaylist playlist:
+                            playlistCollection.Playlists.Add(new PlaylistViewModel(playlist));
+                            break;
+                        case IPlaylistCollection collection:
+                            playlistCollection.Playlists.Add(new PlaylistCollectionViewModel(collection));
+                            break;
+                    }
                 }
             }
         }
