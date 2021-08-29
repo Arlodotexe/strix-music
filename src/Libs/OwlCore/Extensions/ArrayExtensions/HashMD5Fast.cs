0d93a84c40605e17e915db561b031c60841be356
Louis Salkeld <louissalkeld@outlook.com>
2021-08-29 01:27:36 +0100

    Implement new HashMD5Fast overload for byte arrays

---
+++ src/Libs/OwlCore/Extensions/ArrayExtensions/HashMD5Fast.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace OwlCore.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="Array"/>s.
+    /// </summary>
+    public static partial class ArrayExtensions
+    {
+        /// <summary>
+        /// Hash a byte array with MD5, using 16 bytes instead of 32 for speed. Collisions are more likely.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <returns>A 16-bit MD5 hash.</returns>
+        public static string HashMD5Fast(this byte[] array)
+        {
+            unchecked
+            {
+                int hash = 23;
+
+                foreach (byte c in array)
+                {
+                    hash = hash * 31 + c;
+                }
+
+                return Convert.ToString(hash, 16);
+            }
+        }
+    }
+}
