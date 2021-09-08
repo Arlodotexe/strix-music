f9450eedc59f8a66c7cf3fed58ff5a3c52aae532
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-09-08 11:07:49 +0000

    Reordered method so cancellation token variant appears first.

--- src/Libs/OwlCore/Flow/EventAsTask.cs
+++ src/Libs/OwlCore/Flow/EventAsTask.cs
@@ -11,6 +11,62 @@ namespace OwlCore
         /// Waits for an event to fire. If the event doesn't fire within the given timeout, a default value is returned.
         /// </summary>
         /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static async Task<(object? Sender, TResult Result)?> EventAsTask<TResult>(Action<EventHandler<TResult>> subscribe, Action<EventHandler<TResult>> unsubscribe, CancellationToken cancellationToken)
+        {
+            var completionSource = new TaskCompletionSource<(object? Sender, TResult Result)>();
+
+            subscribe(EventHandler);
+
+            try
+            {
+                var result = await Task.Run(() => completionSource.Task, cancellationToken);
+                unsubscribe(EventHandler);
+                return result;
+            }
+            catch (TaskCanceledException)
+            {
+                unsubscribe(EventHandler);
+                return null;
+            }
+
+            void EventHandler(object sender, TResult eventArgs)
+            {
+                completionSource.SetResult((sender, eventArgs));
+            }
+        }
+
+        /// <summary>
+        /// Waits for an event to fire. If the event doesn't fire within the given timeout, a default value is returned.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static async Task<(object? Sender, EventArgs Result)?> EventAsTask(Action<EventHandler> subscribe, Action<EventHandler> unsubscribe, CancellationToken cancellationToken)
+        {
+            var completionSource = new TaskCompletionSource<(object? Sender, EventArgs Result)>();
+
+            subscribe(EventHandler);
+
+            try
+            {
+                var result = await Task.Run(() => completionSource.Task, cancellationToken);
+                unsubscribe(EventHandler);
+                return result;
+            }
+            catch (TaskCanceledException)
+            {
+                unsubscribe(EventHandler);
+                return null;
+            }
+
+            void EventHandler(object sender, EventArgs eventArgs)
+            {
+                completionSource.SetResult((sender, eventArgs));
+            }
+        }
+
+        /// <summary>
+        /// Waits for an event to fire. If the event doesn't fire within the given timeout, a default value is returned.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
         public static async Task<(object? Sender, TResult Result)?> EventAsTask<TResult>(Action<EventHandler<TResult>> subscribe, Action<EventHandler<TResult>> unsubscribe, TimeSpan timeout)
         {
             var completionSource = new TaskCompletionSource<(object? Sender, TResult Result)>();
@@ -52,62 +108,6 @@ namespace OwlCore
             try
             {
                 var result = await Task.Run(() => completionSource.Task, resultCancellationToken.Token);
-                unsubscribe(EventHandler);
-                return result;
-            }
-            catch (TaskCanceledException)
-            {
-                unsubscribe(EventHandler);
-                return null;
-            }
-
-            void EventHandler(object sender, EventArgs eventArgs)
-            {
-                completionSource.SetResult((sender, eventArgs));
-            }
-        }
-
-        /// <summary>
-        /// Waits for an event to fire. If the event doesn't fire within the given timeout, a default value is returned.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
-        public static async Task<(object? Sender, TResult Result)?> EventAsTask<TResult>(Action<EventHandler<TResult>> subscribe, Action<EventHandler<TResult>> unsubscribe, CancellationToken cancellationToken)
-        {
-            var completionSource = new TaskCompletionSource<(object? Sender, TResult Result)>();
-
-            subscribe(EventHandler);
-
-            try
-            {
-                var result = await Task.Run(() => completionSource.Task, cancellationToken);
-                unsubscribe(EventHandler);
-                return result;
-            }
-            catch (TaskCanceledException)
-            {
-                unsubscribe(EventHandler);
-                return null;
-            }
-
-            void EventHandler(object sender, TResult eventArgs)
-            {
-                completionSource.SetResult((sender, eventArgs));
-            }
-        }
-
-        /// <summary>
-        /// Waits for an event to fire. If the event doesn't fire within the given timeout, a default value is returned.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
-        public static async Task<(object? Sender, EventArgs Result)?> EventAsTask(Action<EventHandler> subscribe, Action<EventHandler> unsubscribe, CancellationToken cancellationToken)
-        {
-            var completionSource = new TaskCompletionSource<(object? Sender, EventArgs Result)>();
-
-            subscribe(EventHandler);
-
-            try
-            {
-                var result = await Task.Run(() => completionSource.Task, cancellationToken);
                 unsubscribe(EventHandler);
                 return result;
             }
