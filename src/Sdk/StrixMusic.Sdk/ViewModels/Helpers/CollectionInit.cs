18034e4c4d594383e6a53fa3019806b51ed339a0
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-09-20 14:24:07 +0000

    Gracefully exit if collection init fails

--- src/Sdk/StrixMusic.Sdk/ViewModels/Helpers/CollectionInit.cs
+++ src/Sdk/StrixMusic.Sdk/ViewModels/Helpers/CollectionInit.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Toolkit.Diagnostics;
@@ -22,9 +23,20 @@ namespace StrixMusic.Sdk.ViewModels.Helpers
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task TrackCollection(ITrackCollectionViewModel trackCollection)
         {
+            var lastItemCount = trackCollection.Tracks.Count;
+
             while (trackCollection.Tracks.Count < trackCollection.TotalTrackCount)
             {
                 await trackCollection.PopulateMoreTracksAsync(trackCollection.TotalTrackCount);
+
+                // nothing was returned
+                if (lastItemCount == trackCollection.Tracks.Count)
+                {
+                    Debug.WriteLine($"Warning: Collection init for {nameof(trackCollection)} {trackCollection.Name} failed. Not all items were returned.");
+                    return;
+                }
+
+                lastItemCount = trackCollection.Tracks.Count;
             }
         }
 
@@ -35,9 +47,20 @@ namespace StrixMusic.Sdk.ViewModels.Helpers
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task AlbumCollection(IAlbumCollectionViewModel albumCollection)
         {
+            var lastItemCount = albumCollection.Albums.Count;
+
             while (albumCollection.Albums.Count < albumCollection.TotalAlbumItemsCount)
             {
                 await albumCollection.PopulateMoreAlbumsAsync(albumCollection.TotalAlbumItemsCount);
+
+                // nothing was returned
+                if (lastItemCount == albumCollection.Albums.Count)
+                {
+                    Debug.WriteLine($"Warning: Collection init for {nameof(albumCollection)} {albumCollection.Name} failed. Not all items were returned.");
+                    return;
+                }
+
+                lastItemCount = albumCollection.Albums.Count;
             }
         }
 
@@ -48,9 +71,20 @@ namespace StrixMusic.Sdk.ViewModels.Helpers
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task ArtistCollection(IArtistCollectionViewModel artistCollection)
         {
+            var lastItemCount = artistCollection.Artists.Count;
+
             while (artistCollection.Artists.Count < artistCollection.TotalArtistItemsCount)
             {
                 await artistCollection.PopulateMoreArtistsAsync(artistCollection.TotalArtistItemsCount);
+
+                // nothing was returned
+                if (lastItemCount == artistCollection.Artists.Count)
+                {
+                    Debug.WriteLine($"Warning: Collection init for {nameof(artistCollection)} {artistCollection.Name} failed. Not all items were returned.");
+                    return;
+                }
+
+                lastItemCount = artistCollection.Artists.Count;
             }
         }
 
@@ -61,9 +95,20 @@ namespace StrixMusic.Sdk.ViewModels.Helpers
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task PlaylistCollection(IPlaylistCollectionViewModel playlistCollection)
         {
+            var lastItemCount = playlistCollection.Playlists.Count;
+
             while (playlistCollection.Playlists.Count < playlistCollection.TotalPlaylistItemsCount)
             {
                 await playlistCollection.PopulateMorePlaylistsAsync(playlistCollection.TotalPlaylistItemsCount);
+
+                // nothing was returned
+                if (lastItemCount == playlistCollection.Playlists.Count)
+                {
+                    Debug.WriteLine($"Warning: Collection init for {nameof(playlistCollection)} {playlistCollection.Name} failed. Not all items were returned.");
+                    return;
+                }
+
+                lastItemCount = playlistCollection.Playlists.Count;
             }
         }
     }
