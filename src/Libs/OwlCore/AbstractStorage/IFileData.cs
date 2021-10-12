e097cdce6504fee0c42ed04abf779fae085acad7
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-10-12 11:00:13 +0000

    Updates and cleanup to IFileScanner

--- src/Libs/OwlCore/AbstractStorage/IFileData.cs
+++ src/Libs/OwlCore/AbstractStorage/IFileData.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace OwlCore.AbstractStorage
 {
     /// <summary>
-    /// Holds information about a file
+    /// Represents a file on a file system.
     /// </summary>
     public interface IFileData
     {
