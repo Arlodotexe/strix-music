d748ccf2f6570e2213fe887b5b55d330426a6fe1
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-10-12 10:08:17 +0000

    Massively improved the inbox NewtonsoftRemoteMessageConverter

--- src/Libs/OwlCore/Remoting/Transfer/MessageConverters/NewtonsoftRemoteMessageConverter.cs
+++ src/Libs/OwlCore/Remoting/Transfer/MessageConverters/NewtonsoftRemoteMessageConverter.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Toolkit.Diagnostics;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
+using System.Collections;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Toolkit.Diagnostics;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace OwlCore.Remoting.Transfer.MessageConverters
 {
@@ -13,34 +15,40 @@ namespace OwlCore.Remoting.Transfer.MessageConverters
     /// </summary>
     public class NewtonsoftRemoteMessageConverter : IRemoteMessageConverter
     {
+        private JsonSerializerSettings _jsonSerializerSettings;
+
         /// <summary>
-        /// The settings used when serializing and deserializing json.
+        /// Creates an instance of <see cref="NewtonsoftRemoteMessageConverter"/>.
         /// </summary>
-        public JsonSerializerSettings SerializerSettings { get; set; } = new JsonSerializerSettings
+        public NewtonsoftRemoteMessageConverter()
         {
-            ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
-            PreserveReferencesHandling = PreserveReferencesHandling.All,
-            TypeNameHandling = TypeNameHandling.All,
-            TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Full,
-        };
+            _jsonSerializerSettings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                TypeNameHandling = TypeNameHandling.All,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Full,
+                MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateParseHandling = DateParseHandling.DateTime,
+                FloatFormatHandling = FloatFormatHandling.String,
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            };
+        }
 
         /// <inheritdoc/>
-        public Task<IRemoteMessage> DeserializeAsync(byte[] message, CancellationToken? cancellationToken = null)
+        public async Task<IRemoteMessage> DeserializeAsync(byte[] message, CancellationToken? cancellationToken = null)
         {
-            var jsonStr = Encoding.UTF8.GetString(message);
-            Guard.IsNotNullOrWhiteSpace(jsonStr, nameof(jsonStr));
-
-            var deserializedBase = JsonConvert.DeserializeObject<RemoteMessageBase>(jsonStr, SerializerSettings);
+            var deserializedBase = await DeserializeAsync<IRemoteMessage>();
             Guard.IsNotNull(deserializedBase, nameof(deserializedBase));
 
-            IRemoteMessage result = deserializedBase.Action switch
+            IRemoteMessage? result = deserializedBase.Action switch
             {
                 RemotingAction.None => deserializedBase,
-                RemotingAction.MethodCall => JsonConvert.DeserializeObject<RemoteMethodCallMessage>(jsonStr),
-                RemotingAction.PropertyChange => JsonConvert.DeserializeObject<RemotePropertyChangeMessage>(jsonStr),
-                RemotingAction.RemoteDataProxy => JsonConvert.DeserializeObject<RemoteDataMessage>(jsonStr),
-                RemotingAction.ExceptionThrown => JsonConvert.DeserializeObject<RemoteExceptionDataMessage>(jsonStr),
-                _ => ThrowHelper.ThrowNotSupportedException<IRemoteMemberMessage>(),
+                RemotingAction.MethodCall => await DeserializeAsync<RemoteMethodCallMessage>(),
+                RemotingAction.PropertyChange => await DeserializeAsync<RemotePropertyChangeMessage>(),
+                RemotingAction.RemoteDataProxy => await DeserializeAsync<RemoteDataMessage>(),
+                RemotingAction.ExceptionThrown => await DeserializeAsync<RemoteExceptionDataMessage>(),
+                _ => ThrowHelper.ThrowNotSupportedException<IRemoteMessage>(),
             };
 
             if (result is RemoteMethodCallMessage memberMessage)
@@ -55,8 +63,9 @@ namespace OwlCore.Remoting.Transfer.MessageConverters
                         param.Value = container.ToObject(targetType);
                     }
 
-                    if (Guid.TryParse(param.Value?.ToString(), out Guid guid))
-                        param.Value = guid;
+                    Guard.IsNotNullOrWhiteSpace(param.AssemblyQualifiedName, nameof(param.AssemblyQualifiedName));
+
+                    param.Value = TrySmartTypeConversion(param.Value, param.AssemblyQualifiedName);
                 }
             }
 
@@ -68,11 +77,24 @@ namespace OwlCore.Remoting.Transfer.MessageConverters
                     dataMsg.Result = container.ToObject(targetType);
                 }
 
-                if (Guid.TryParse(dataMsg.Result?.ToString(), out Guid guid))
-                    dataMsg.Result = guid;
+                dataMsg.Result = TrySmartTypeConversion(dataMsg.Result, dataMsg.TargetMemberSignature);
             }
 
-            return Task.FromResult(result);
+            if (result is RemotePropertyChangeMessage remoteProp)
+            {
+                remoteProp.NewValue = TrySmartTypeConversion(remoteProp.NewValue, remoteProp.TargetMemberSignature);
+                remoteProp.OldValue = TrySmartTypeConversion(remoteProp.OldValue, remoteProp.TargetMemberSignature);
+            }
+
+            Guard.IsNotNull(result, nameof(result));
+            return result;
+
+            Task<T?> DeserializeAsync<T>()
+            {
+                var str = Encoding.UTF8.GetString(message);
+
+                return Task.FromResult(JsonConvert.DeserializeObject<T>(str, _jsonSerializerSettings));
+            }
         }
 
         /// <inheritdoc/>
@@ -84,14 +106,115 @@ namespace OwlCore.Remoting.Transfer.MessageConverters
             if (methodCallMessage != null)
                 methodCallMessage.TargetMemberSignature = $"TARGETNAME_{methodCallMessage.TargetMemberSignature}";
 
-            var jsonStr = JsonConvert.SerializeObject(message, SerializerSettings);
-            var bytes = Encoding.UTF8.GetBytes(jsonStr);
+            using var stream = new MemoryStream();
+            var str = JsonConvert.SerializeObject(message, _jsonSerializerSettings);
 
             // Newtonsoft won't serialize a string containing a method signature.
             if (methodCallMessage != null)
                 methodCallMessage.TargetMemberSignature = methodCallMessage.TargetMemberSignature.Replace("TARGETNAME_", "");
 
-            return Task.FromResult(bytes);
+            return Task.FromResult(Encoding.UTF8.GetBytes(str));
+        }
+
+        /// <summary>
+        /// A reflection-free way to handle additional type conversion not properly handled by newtonsoft.
+        /// </summary>
+        private object? TrySmartTypeConversion(object? value, string assemblyQualifiedName)
+        {
+            value = TrySmartTypeConversion_Primitives(value, assemblyQualifiedName);
+            value = TrySmartTypeConversion_Structs(value, assemblyQualifiedName);
+            value = TrySmartTypeConversion_EnumerableItems(value);
+
+            return value;
+        }
+
+        private object? TrySmartTypeConversion_EnumerableItems(object? value)
+        {
+            if (!(value is string) && value is IEnumerable collection)
+                return TrySmartTypeConversion_EnumerableItems_Internal(collection);
+
+            return value;
+
+            // The only sane way to do this without going completely overboard on possible collection types.
+            IEnumerable TrySmartTypeConversion_EnumerableItems_Internal(IEnumerable enumerable)
+            {
+                foreach (var item in enumerable)
+                    yield return TrySmartTypeConversion(item, item?.GetType().AssemblyQualifiedName ?? string.Empty);
+            }
+        }
+
+        private object? TrySmartTypeConversion_Structs(object? value, string assemblyQualifiedName)
+        {
+            if (value is string valStr)
+            {
+                if (assemblyQualifiedName == typeof(TimeSpan).AssemblyQualifiedName && TimeSpan.TryParse(valStr, out var timeSpan))
+                    return timeSpan;
+
+                if (assemblyQualifiedName == typeof(DateTime).AssemblyQualifiedName && DateTime.TryParse(valStr, out var dateTime))
+                    return dateTime;
+
+                if (assemblyQualifiedName == typeof(Guid).AssemblyQualifiedName && Guid.TryParse(valStr, out var guid))
+                    return guid;
+            }
+
+            return value;
+        }
+
+        private object? TrySmartTypeConversion_Primitives(object? value, string assemblyQualifiedName)
+        {
+            if (assemblyQualifiedName == typeof(short).AssemblyQualifiedName)
+                return Convert.ToInt16(value);
+
+            if (assemblyQualifiedName == typeof(ushort).AssemblyQualifiedName)
+                return Convert.ToUInt16(value);
+
+            if (assemblyQualifiedName == typeof(int).AssemblyQualifiedName)
+                return Convert.ToInt32(value);
+
+            if (assemblyQualifiedName == typeof(uint).AssemblyQualifiedName)
+                return Convert.ToUInt32(value);
+
+            if (assemblyQualifiedName == typeof(double).AssemblyQualifiedName)
+            {
+                if (value is string doubleStr)
+                {
+                    if (doubleStr == "Infinity")
+                        return double.PositiveInfinity;
+
+                    if (doubleStr == "-Infinity")
+                        return double.NegativeInfinity;
+
+                    if (doubleStr == "NaN")
+                        return double.NaN;
+                }
+
+                return Convert.ToDouble(value);
+            }
+
+            if (assemblyQualifiedName == typeof(float).AssemblyQualifiedName)
+            {
+                if (value is string floatStr)
+                {
+                    if (floatStr == "Infinity")
+                        return float.PositiveInfinity;
+
+                    if (floatStr == "-Infinity")
+                        return float.NegativeInfinity;
+
+                    if (floatStr == "NaN")
+                        return float.NaN;
+                }
+
+                return Convert.ToSingle(value);
+            }
+
+            if (assemblyQualifiedName == typeof(byte).AssemblyQualifiedName)
+                return Convert.ToByte(value);
+
+            if (assemblyQualifiedName == typeof(sbyte).AssemblyQualifiedName)
+                return Convert.ToSByte(value);
+
+            return value;
         }
     }
 }
