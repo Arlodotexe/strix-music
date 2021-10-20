2b0569dbf9ce29f0426cdd747991cd06469d192d
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-10-20 18:00:15 +0000

    Added CachedRequestFound and CachedRequestSaving relay from CachedHttpClientHandlerAction to CachedHttpClientHandler

--- src/Libs/OwlCore/Net/HttpClientHandlers/CachedHttpClientHandler.cs
+++ src/Libs/OwlCore/Net/HttpClientHandlers/CachedHttpClientHandler.cs
@@ -18,12 +18,44 @@ namespace OwlCore.Net.HttpClientHandlers
         public CachedHttpClientHandler(string cacheFolderPath, TimeSpan defaultCacheTime)
         {
             _cachedHttpClientHandler = new CachedHttpClientHandlerAction(cacheFolderPath, defaultCacheTime);
+
+            _cachedHttpClientHandler.CachedRequestFound += OnCachedRequestFound;
+            _cachedHttpClientHandler.CachedRequestSaving += OnCachedRequestSaving;
         }
+
+        private void OnCachedRequestSaving(object sender, CachedRequestEventArgs e)
+        {
+            CachedRequestSaving?.Invoke(sender, e);
+        }
+
+        private void OnCachedRequestFound(object sender, CachedRequestEventArgs e)
+        {
+            CachedRequestFound?.Invoke(sender, e);
+        }
+
+        /// <summary>
+        /// Raised when cache is found and is about to be used.
+        /// </summary>
+        public event EventHandler<CachedRequestEventArgs>? CachedRequestFound;
+
+        /// <summary>
+        /// Raised when new data is retrieved and is about to be saved.
+        /// </summary>
+        public event EventHandler<CachedRequestEventArgs>? CachedRequestSaving;
 
         /// <inheritdoc />
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             return _cachedHttpClientHandler.SendAsync(request, cancellationToken, () => base.SendAsync(request, cancellationToken));
+        }
+
+        /// <inheritdoc/>
+        protected override void Dispose(bool disposing)
+        {
+            _cachedHttpClientHandler.CachedRequestFound -= OnCachedRequestFound;
+            _cachedHttpClientHandler.CachedRequestSaving -= OnCachedRequestSaving;
+
+            base.Dispose(disposing);
         }
     }
 }
