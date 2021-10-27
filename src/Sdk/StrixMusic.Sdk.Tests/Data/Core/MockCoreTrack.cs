9ce7efae857f5a098cc3e6868388653fb5dcd0ec
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-10-27 21:41:22 +0000

    Placeholder setup of unit tests for PlaybackHandlerService and Shuffle

---
+++ src/Sdk/StrixMusic.Sdk.Tests/Data/Core/MockCoreTrack.cs
@@ -0,0 +1,260 @@
+﻿using OwlCore.Events;
+using StrixMusic.Sdk.Data;
+using StrixMusic.Sdk.Data.Core;
+using StrixMusic.Sdk.MediaPlayback;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrixMusic.Sdk.Tests.Data.Core
+{
+    internal class MockCoreTrack : ICoreTrack
+    {
+        public ICoreAlbum Album => throw new NotImplementedException();
+
+        public ICoreLyrics Lyrics => throw new NotImplementedException();
+
+        public ICorePlayableCollectionGroup RelatedItems => throw new NotImplementedException();
+
+        public TrackType Type => throw new NotImplementedException();
+
+        public int? TrackNumber => throw new NotImplementedException();
+
+        public int? DiscNumber => throw new NotImplementedException();
+
+        public CultureInfo Language => throw new NotImplementedException();
+
+        public bool IsExplicit => throw new NotImplementedException();
+
+        public bool IsChangeAlbumAsyncAvailable => throw new NotImplementedException();
+
+        public bool IsChangeTrackNumberAsyncAvailable => throw new NotImplementedException();
+
+        public bool IsChangeLanguageAsyncAvailable => throw new NotImplementedException();
+
+        public bool IsChangeLyricsAsyncAvailable => throw new NotImplementedException();
+
+        public bool IsChangeIsExplicitAsyncAvailable => throw new NotImplementedException();
+
+        public int TotalArtistItemsCount => throw new NotImplementedException();
+
+        public bool IsPlayArtistCollectionAsyncAvailable => throw new NotImplementedException();
+
+        public bool IsPauseArtistCollectionAsyncAvailable => throw new NotImplementedException();
+
+        public DateTime? AddedAt => throw new NotImplementedException();
+
+        public string Id => throw new NotImplementedException();
+
+        public string Name => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public DateTime? LastPlayed => throw new NotImplementedException();
+
+        public PlaybackState PlaybackState => throw new NotImplementedException();
+
+        public TimeSpan Duration => throw new NotImplementedException();
+
+        public bool IsChangeNameAsyncAvailable => throw new NotImplementedException();
+
+        public bool IsChangeDescriptionAsyncAvailable => throw new NotImplementedException();
+
+        public bool IsChangeDurationAsyncAvailable => throw new NotImplementedException();
+
+        public int TotalImageCount => throw new NotImplementedException();
+
+        public int TotalUrlCount => throw new NotImplementedException();
+
+        public int TotalGenreCount => throw new NotImplementedException();
+
+        public ICore SourceCore => throw new NotImplementedException();
+
+        public event EventHandler<ICoreAlbum> AlbumChanged;
+        public event EventHandler<ICoreLyrics> LyricsChanged;
+        public event EventHandler<int?> TrackNumberChanged;
+        public event EventHandler<CultureInfo> LanguageChanged;
+        public event EventHandler<bool> IsExplicitChanged;
+        public event CollectionChangedEventHandler<ICoreArtistCollectionItem> ArtistItemsChanged;
+        public event EventHandler<bool> IsPlayArtistCollectionAsyncAvailableChanged;
+        public event EventHandler<bool> IsPauseArtistCollectionAsyncAvailableChanged;
+        public event EventHandler<int> ArtistItemsCountChanged;
+        public event CollectionChangedEventHandler<ICoreImage> ImagesChanged;
+        public event CollectionChangedEventHandler<ICoreUrl> UrlsChanged;
+        public event EventHandler<PlaybackState> PlaybackStateChanged;
+        public event EventHandler<string> NameChanged;
+        public event EventHandler<string> DescriptionChanged;
+        public event EventHandler<TimeSpan> DurationChanged;
+        public event EventHandler<DateTime?> LastPlayedChanged;
+        public event EventHandler<bool> IsChangeNameAsyncAvailableChanged;
+        public event EventHandler<bool> IsChangeDescriptionAsyncAvailableChanged;
+        public event EventHandler<bool> IsChangeDurationAsyncAvailableChanged;
+        public event EventHandler<int> ImagesCountChanged;
+        public event EventHandler<int> UrlsCountChanged;
+        public event CollectionChangedEventHandler<ICoreGenre> GenresChanged;
+        public event EventHandler<int> GenresCountChanged;
+
+        public Task AddArtistItemAsync(ICoreArtistCollectionItem artist, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddGenreAsync(ICoreGenre genre, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddImageAsync(ICoreImage image, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddUrlAsync(ICoreUrl url, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeAlbumAsync(ICoreAlbum albums)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeDescriptionAsync(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeDurationAsync(TimeSpan duration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeIsExplicitAsync(bool isExplicit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeLanguageAsync(CultureInfo language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeLyricsAsync(ICoreLyrics lyrics)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeTrackNumberAsync(int? trackNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<ICoreArtistCollectionItem> GetArtistItemsAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<ICoreGenre> GetGenresAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<ICoreImage> GetImagesAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<ICoreUrl> GetUrlsAsync(int limit, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAddArtistItemAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAddGenreAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAddImageAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAddUrlAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsRemoveArtistItemAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsRemoveGenreAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsRemoveImageAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsRemoveUrlAvailableAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PauseArtistCollectionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PlayArtistCollectionAsync(ICoreArtistCollectionItem artistItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PlayArtistCollectionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveArtistItemAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveGenreAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveImageAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveUrlAsync(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Sdk/StrixMusic.Sdk.Tests/Services/MediaPlayback/PlaybackHandlerService.Tests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StrixMusic.Sdk.Services.MediaPlayback;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrixMusic.Sdk.Tests.Services.MediaPlayback
+{
+    [TestClass]
+    public class PlaybackHandlerServiceTest
+    {
+        private PlaybackHandlerService _handlerService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Create handler service, pass in mock audio player, mock IMediaSourceConfig, mock coretrack as needed.
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // dispose media players, handler services, tracks, etc.
+        }
+
+        public void ShuffleQueue_AddMoreOfMe()
+        {
+            // TODO
+            // Create playback handler instance
+
+            // Turning on shuffle
+            // Make sure previous items are emptied
+            // Make sure all items from previous and next exist in next after shuffle
+            // Make sure shuffle actually shuffles (no sequential items)
+            // Needs to complete within 800ms
+            // etc
+
+            // Turning off shuffle
+            // Needs to restore the queue exactly how it was
+            // Needs to complete within 800ms
+            // etc
+
+            // Both on / off
+            // Make sure current item doesn't change when toggling shuffle.
+        }
+    }
+}
