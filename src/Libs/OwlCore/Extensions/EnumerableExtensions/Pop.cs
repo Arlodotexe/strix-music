1897d0963ddb8186f3b7b789299a0243df2bccc9
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-10-27 20:43:16 +0000

    Added Pop and ReplaceOrAdd

---
+++ src/Libs/OwlCore/Extensions/EnumerableExtensions/Pop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.Toolkit.Diagnostics;
+
+namespace OwlCore.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        /// <summary>
+        /// Removes and returns the last item in the <paramref name="source"/>.
+        /// </summary>
+        /// <param name="source">The collection to modify.</param>
+        /// <typeparam name="T">The type of the item being returned.</typeparam>
+        public static T Pop<T>(this IList<T> source)
+        {
+            var lastIndex = source.Count - 1;
+            var itemToReturn = source[lastIndex];
+
+            source.RemoveAt(lastIndex);
+            return itemToReturn;
+        }
+    }
+}
