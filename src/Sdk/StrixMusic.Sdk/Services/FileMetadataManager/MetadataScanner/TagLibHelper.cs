8347ab6d2eb161796a21dce2a2f3e526a6bcca07
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-11-07 15:23:25 +0000

    Added remaning taglib mimetypes for extension support

--- src/Sdk/StrixMusic.Sdk/Services/FileMetadataManager/MetadataScanner/TagLibHelper.cs
+++ src/Sdk/StrixMusic.Sdk/Services/FileMetadataManager/MetadataScanner/TagLibHelper.cs
@@ -1,7 +1,4 @@
-﻿using OwlCore.Validation.Mime;
-using System.IO;
-
-namespace StrixMusic.Sdk.Services.FileMetadataManager.MetadataScanner
+﻿namespace StrixMusic.Sdk.Services.FileMetadataManager.MetadataScanner
 {
     /// <summary>
     /// Miscellaneous helpers for <see cref="TagLib"/>.
@@ -20,85 +17,107 @@ namespace StrixMusic.Sdk.Services.FileMetadataManager.MetadataScanner
 
             _manualFileTypeResolversAdded = true;
 
-            // The last added resolver gets run first.
-            // Resolve by file extension as fallback.
-            TagLib.File.AddFileTypeResolver(ResolveByFileExtension);
-            TagLib.File.AddFileTypeResolver(ResolveByMimeType);
-
-            TagLib.File? ResolveByFileExtension(TagLib.File.IFileAbstraction abstr, string mimeType, TagLib.ReadStyle readStyle)
-            {
-                var extension = Path.GetExtension(abstr.Name);
-                if (string.IsNullOrWhiteSpace(extension))
-                    return null;
-
-                // TODO: Use TagLib extension-to-file mapping to guaruntee compatability.
-                // Not all platforms supply the actual mime type.
-                // Fall back to getting mime type from the file extension.
-                if (MimeTypeMap.TryGetMimeType(extension, out var detectedMimeType))
-                    return ResolveByMimeType(abstr, detectedMimeType, readStyle);
-
-                return null;
-            }
-
-            TagLib.File? ResolveByMimeType(TagLib.File.IFileAbstraction abstr, string mimeType, TagLib.ReadStyle readStyle) => mimeType switch
+            // Mirrors the [SupportedMimeType] attribute that TagLib puts on each type.
+            TagLib.File.AddFileTypeResolver((abstr, mimeType, readStyle) => mimeType switch
             {
                 // Aac
+                "taglib/aac" => new TagLib.Aac.File(abstr, readStyle),
                 "audio/aac" => new TagLib.Aac.File(abstr, readStyle),
 
                 // Aiff
+                "taglib/aif" => new TagLib.Aiff.File(abstr, readStyle),
+                "taglib/aiff" => new TagLib.Aiff.File(abstr, readStyle),
                 "audio/x-aiff" => new TagLib.Aiff.File(abstr, readStyle),
                 "audio/aiff" => new TagLib.Aiff.File(abstr, readStyle),
                 "sound/aiff" => new TagLib.Aiff.File(abstr, readStyle),
                 "application/x-aiff" => new TagLib.Aiff.File(abstr, readStyle),
 
                 // Ape
+                "taglib/ape" => new TagLib.Ape.File(abstr, readStyle),
                 "audio/x-ape" => new TagLib.Ape.File(abstr, readStyle),
                 "audio/ape" => new TagLib.Ape.File(abstr, readStyle),
                 "application/x-ape" => new TagLib.Ape.File(abstr, readStyle),
 
                 // Asf
+                "taglib/wma" => new TagLib.Asf.File(abstr, readStyle),
+                "taglib/wmv" => new TagLib.Asf.File(abstr, readStyle),
+                "taglib/asf" => new TagLib.Asf.File(abstr, readStyle),
                 "audio/x-ms-wma" => new TagLib.Asf.File(abstr, readStyle),
                 "audio/x-ms-asf" => new TagLib.Asf.File(abstr, readStyle),
                 "video/x-ms-asf" => new TagLib.Asf.File(abstr, readStyle),
 
                 // Audible
+                "taglib/aa" => new TagLib.Audible.File(abstr, readStyle),
+                "taglib/aax" => new TagLib.Audible.File(abstr, readStyle),
                 "audio/x-audible" => new TagLib.Audible.File(abstr, readStyle),
 
                 // Dsf
+                "taglib/dsf" => new TagLib.Dsf.File(abstr, readStyle),
                 "audio/x-dsf" => new TagLib.Dsf.File(abstr, readStyle),
                 "audio/dsf" => new TagLib.Dsf.File(abstr, readStyle),
                 "sound/dsf" => new TagLib.Dsf.File(abstr, readStyle),
                 "application/x-dsf" => new TagLib.Dsf.File(abstr, readStyle),
 
                 // Flac
+                "taglib/flac" => new TagLib.Flac.File(abstr, readStyle),
                 "audio/x-flac" => new TagLib.Flac.File(abstr, readStyle),
                 "application/x-flac" => new TagLib.Flac.File(abstr, readStyle),
                 "audio/flac" => new TagLib.Flac.File(abstr, readStyle),
 
                 // gif
+                "taglib/gif" => new TagLib.Gif.File(abstr, readStyle),
                 "image/gif" => new TagLib.Gif.File(abstr, readStyle),
 
                 // Image.NoMetadata
+                "taglib/bmp" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/x-MS-bmp" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/x-bmp" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/ppm" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/pgm" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/pbm" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/pnm" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/x-portable-pixmap" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/x-portable-graymap" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/x-portable-bitmap" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/x-portable-anymap" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/pcx" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/svg" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/svgz" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/x-pcx" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
                 "image/svg+xml" => new TagLib.Image.NoMetadata.File(abstr, readStyle),
+                "taglib/kdc" => new TagLib.Image.NoMetadata.File(abstr, readStyle),    // FIXME: Not supported yet!
+                "taglib/orf" => new TagLib.Image.NoMetadata.File(abstr, readStyle),    // FIXME: Not supported yet!
+                "taglib/srf" => new TagLib.Image.NoMetadata.File(abstr, readStyle),    // FIXME: Not supported yet!
+                "taglib/crw" => new TagLib.Image.NoMetadata.File(abstr, readStyle),    // FIXME: Not supported yet!
+                "taglib/mrw" => new TagLib.Image.NoMetadata.File(abstr, readStyle),    // FIXME: Not supported yet!
+                "taglib/raf" => new TagLib.Image.NoMetadata.File(abstr, readStyle),    // FIXME: Not supported yet!
+                "taglib/x3f" => new TagLib.Image.NoMetadata.File(abstr, readStyle),    // FIXME: Not supported yet!
 
                 // Jpeg
                 "image/jpeg" => new TagLib.Jpeg.File(abstr, readStyle),
+                "taglib/jpg" => new TagLib.Jpeg.File(abstr, readStyle),
+                "taglib/jpeg" => new TagLib.Jpeg.File(abstr, readStyle),
+                "taglib/jpe" => new TagLib.Jpeg.File(abstr, readStyle),
+                "taglib/jif" => new TagLib.Jpeg.File(abstr, readStyle),
+                "taglib/jfif" => new TagLib.Jpeg.File(abstr, readStyle),
+                "taglib/jfi" => new TagLib.Jpeg.File(abstr, readStyle),
 
                 // Matroska
+                "taglib/mkv" => new TagLib.Matroska.File(abstr, readStyle),
+                "taglib/mka" => new TagLib.Matroska.File(abstr, readStyle),
+                "taglib/mks" => new TagLib.Matroska.File(abstr, readStyle),
+                "taglib/webm" => new TagLib.Matroska.File(abstr, readStyle),
                 "video/webm" => new TagLib.Matroska.File(abstr, readStyle),
                 "video/x-matroska" => new TagLib.Matroska.File(abstr, readStyle),
 
                 // MusePack
+                "taglib/mpc" => new TagLib.MusePack.File(abstr, readStyle),
+                "taglib/mp+" => new TagLib.MusePack.File(abstr, readStyle),
+                "taglib/mpp" => new TagLib.MusePack.File(abstr, readStyle),
                 "audio/x-musepack" => new TagLib.MusePack.File(abstr, readStyle),
 
                 // Mpeg
+                "taglib/mp3" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
                 "audio/x-mp3" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
                 "application/x-id3" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
                 "audio/mpeg" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
@@ -106,20 +125,35 @@ namespace StrixMusic.Sdk.Services.FileMetadataManager.MetadataScanner
                 "audio/x-mpeg-3" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
                 "audio/mpeg3" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
                 "audio/mp3" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
+                "taglib/m2a" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
+                "taglib/mp2" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
+                "taglib/mp1" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
                 "audio/x-mp2" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
                 "audio/x-mp1" => new TagLib.Mpeg.AudioFile(abstr, readStyle),
 
+                "taglib/mpg" => new TagLib.Mpeg.File(abstr, readStyle),
+                "taglib/mpeg" => new TagLib.Mpeg.File(abstr, readStyle),
+                "taglib/mpe" => new TagLib.Mpeg.File(abstr, readStyle),
+                "taglib/mpv2" => new TagLib.Mpeg.File(abstr, readStyle),
+                "taglib/m2v" => new TagLib.Mpeg.File(abstr, readStyle),
                 "video/x-mpg" => new TagLib.Mpeg.File(abstr, readStyle),
                 "video/mpeg" => new TagLib.Mpeg.File(abstr, readStyle),
 
                 // Mpeg4
+                "taglib/m4a" => new TagLib.Mpeg4.File(abstr, readStyle),
+                "taglib/m4b" => new TagLib.Mpeg4.File(abstr, readStyle),
+                "taglib/m4v" => new TagLib.Mpeg4.File(abstr, readStyle),
+                "taglib/m4p" => new TagLib.Mpeg4.File(abstr, readStyle),
                 "audio/mp4" => new TagLib.Mpeg4.File(abstr, readStyle),
                 "audio/x-m4a" => new TagLib.Mpeg4.File(abstr, readStyle),
                 "video/mp4" => new TagLib.Mpeg4.File(abstr, readStyle),
                 "video/x-m4v" => new TagLib.Mpeg4.File(abstr, readStyle),
 
                 // Ogg
-                "application/ogg" => new TagLib.Ogg.File(abstr, readStyle),
+                "taglib/ogg" => new TagLib.Ogg.File(abstr, readStyle),
+                "taglib/oga" => new TagLib.Ogg.File(abstr, readStyle),
+                "taglib/ogv" => new TagLib.Ogg.File(abstr, readStyle),
+                "taglib/opus" => new TagLib.Ogg.File(abstr, readStyle),
                 "application/x-ogg" => new TagLib.Ogg.File(abstr, readStyle),
                 "audio/vorbis" => new TagLib.Ogg.File(abstr, readStyle),
                 "audio/x-vorbis" => new TagLib.Ogg.File(abstr, readStyle),
@@ -135,9 +169,13 @@ namespace StrixMusic.Sdk.Services.FileMetadataManager.MetadataScanner
                 "audio/x-opus+ogg" => new TagLib.Ogg.File(abstr, readStyle),
 
                 // Png
+                "taglib/png" => new TagLib.Png.File(abstr, readStyle),
                 "image/png" => new TagLib.Png.File(abstr, readStyle),
 
                 // Riff
+                "taglib/avi" => new TagLib.Riff.File(abstr, readStyle),
+                "taglib/wav" => new TagLib.Riff.File(abstr, readStyle),
+                "taglib / divx" => new TagLib.Riff.File(abstr, readStyle),
                 "video/avi" => new TagLib.Riff.File(abstr, readStyle),
                 "video/msvideo" => new TagLib.Riff.File(abstr, readStyle),
                 "video/x-msvideo" => new TagLib.Riff.File(abstr, readStyle),
@@ -149,39 +187,48 @@ namespace StrixMusic.Sdk.Services.FileMetadataManager.MetadataScanner
                 "audio/x-wav" => new TagLib.Riff.File(abstr, readStyle),
 
                 // Tiff.Arw
+                "taglib/arw" => new TagLib.Tiff.Arw.File(abstr, readStyle),
                 "image/arw" => new TagLib.Tiff.Arw.File(abstr, readStyle),
                 "image/x-sony-arw" => new TagLib.Tiff.Arw.File(abstr, readStyle),
 
                 // Tiff.Cr2
+                "taglib/cr2" => new TagLib.Tiff.Cr2.File(abstr, readStyle),
                 "image/cr2" => new TagLib.Tiff.Cr2.File(abstr, readStyle),
                 "image/x-cannon-cr2" => new TagLib.Tiff.Cr2.File(abstr, readStyle),
 
                 // Tiff.Dng
+                "taglib/dng" => new TagLib.Tiff.Dng.File(abstr, readStyle),
                 "image/dng" => new TagLib.Tiff.Dng.File(abstr, readStyle),
                 "image/x-adobe-dng" => new TagLib.Tiff.Dng.File(abstr, readStyle),
 
                 // Tiff.Nef
+                "taglib/nef" => new TagLib.Tiff.Nef.File(abstr, readStyle),
                 "image/nef" => new TagLib.Tiff.Nef.File(abstr, readStyle),
                 "image/x-nikon-nef" => new TagLib.Tiff.Nef.File(abstr, readStyle),
 
                 // Tiff.Pef
+                "taglib/pef" => new TagLib.Tiff.Pef.File(abstr, readStyle),
                 "image/pef" => new TagLib.Tiff.Pef.File(abstr, readStyle),
                 "image/x-pentax-pef" => new TagLib.Tiff.Pef.File(abstr, readStyle),
 
                 // Tiff.Rw2
+                "taglib/rw2" => new TagLib.Tiff.Rw2.File(abstr, readStyle),
                 "image/rw2" => new TagLib.Tiff.Rw2.File(abstr, readStyle),
                 "image/raw" => new TagLib.Tiff.Rw2.File(abstr, readStyle),
+                "taglib/raw" => new TagLib.Tiff.Rw2.File(abstr, readStyle),
                 "image/x-raw" => new TagLib.Tiff.Rw2.File(abstr, readStyle),
                 "image/x-panasonic-raw" => new TagLib.Tiff.Rw2.File(abstr, readStyle),
 
                 // Tiff
+                "taglib/tiff" => new TagLib.Tiff.File(abstr, readStyle),
                 "image/tiff" => new TagLib.Tiff.File(abstr, readStyle),
 
                 // WavPack
+                "taglib/wv" => new TagLib.WavPack.File(abstr, readStyle),
                 "audio/x-wavpack" => new TagLib.WavPack.File(abstr, readStyle),
 
                 _ => throw new System.NotSupportedException($"{mimeType} not supported or mapped."),
-            };
+            });
         }
     }
 }
