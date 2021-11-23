235b7c3a06a629929f84eddc9e7206945dff877e
amaidniazi <amaidhassan@hotmail.com>
2021-11-23 15:28:56 +0000

    WIP - AlbumCollectionViewModel as dependency property refactor.

--- src/Sdk/StrixMusic.Sdk.Uno/Controls/Items/AlbumItem.cs
+++ src/Sdk/StrixMusic.Sdk.Uno/Controls/Items/AlbumItem.cs
@@ -1,5 +1,6 @@
 ﻿using StrixMusic.Sdk.Uno.Controls.Items.Abstract;
 using StrixMusic.Sdk.ViewModels;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace StrixMusic.Sdk.Uno.Controls.Items
@@ -40,8 +41,19 @@ namespace StrixMusic.Sdk.Uno.Controls.Items
         }
 
         /// <summary>
-        /// The <see cref="ArtistViewModel"/> for the control.
+        /// ViewModel holding the data for <see cref="AlbumItem" />
         /// </summary>
-        public AlbumViewModel ViewModel => (AlbumViewModel)DataContext;
+        public AlbumViewModel Album
+        {
+            get { return (AlbumViewModel)GetValue(AlbumProperty); }
+            set { SetValue(AlbumProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency property for <ses cref="AlbumViewModel" />.
+        /// </summary>
+        // Using a DependencyProperty as the backing store for ViewModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AlbumProperty =
+            DependencyProperty.Register(nameof(Album), typeof(AlbumViewModel), typeof(AlbumItem), new PropertyMetadata(0));
     }
 }
