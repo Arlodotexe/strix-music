12598d74b6d5c24b127c954bed3ab4416bbc5886
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-02-05 21:55:39 +0000

    Improved comments

--- src/Sdk/StrixMusic.Sdk/Models/Core/CoreMetadata.cs
+++ src/Sdk/StrixMusic.Sdk/Models/Core/CoreMetadata.cs
@@ -3,16 +3,16 @@
 namespace StrixMusic.Sdk.Models.Core
 {
     /// <summary>
-    /// Contains metadata for a registered core. Used to identify and instantiate a new core instance.
+    /// Contains metadata for a registered core. Used to identify a core before instantiation.
     /// </summary>
     public sealed class CoreMetadata
     {
         /// <summary>
         /// Creates a new instance of <see cref="CoreMetadata"/>.
         /// </summary>
-        /// <param name="id">A unique identifier for this core, including all instances.</param>
+        /// <param name="id">A unique identifier for this core, across all instances.</param>
         /// <param name="displayName">The user-friendly name of the core.</param>
-        /// <param name="logoUri">A local path or url pointing to a SVG file containing the logo for this core.</param>
+        /// <param name="logoUri">A relative path pointing to a SVG file containing the logo for this core.</param>
         /// <param name="sdkVersion">The version of the Strix Music SDK that this core was built against.</param>
         public CoreMetadata(string id, string displayName, Uri logoUri, Version sdkVersion)
         {
@@ -23,17 +23,17 @@ namespace StrixMusic.Sdk.Models.Core
         }
 
         /// <summary>
-        /// A unique identifier for this registered core, regardless of instances.
+        /// A unique identifier for this registered core, across all instances.
         /// </summary>
         public string Id { get; }
 
         /// <summary>
-        /// A local path or url pointing to a SVG file containing the logo for this core.
+        /// A relative path pointing to a SVG file containing the logo for this core.
         /// </summary>
         public Uri LogoUri { get; }
 
         /// <summary>
-        /// The user-friendly name of the core.
+        /// The user-friendly name for the core.
         /// </summary>
         public string DisplayName { get; }
 
