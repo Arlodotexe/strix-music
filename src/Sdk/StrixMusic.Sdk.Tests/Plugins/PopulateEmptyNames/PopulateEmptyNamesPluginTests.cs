c8226fc9313e9be0f99314a719ddc254ac526019
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-02-25 17:59:34 +0000

    Add tests for applying default value via global plugin connector

--- src/Sdk/StrixMusic.Sdk.Tests/Plugins/PopulateEmptyNames/PopulateEmptyNamesPluginTests.cs
+++ src/Sdk/StrixMusic.Sdk.Tests/Plugins/PopulateEmptyNames/PopulateEmptyNamesPluginTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StrixMusic.Sdk.Models.Merged;
+using StrixMusic.Sdk.Plugins.Model;
 using StrixMusic.Sdk.Plugins.PopulateEmptyNames;
 using StrixMusic.Sdk.Tests.Mock;
 using StrixMusic.Sdk.Tests.Mock.Core;
@@ -199,6 +200,70 @@ namespace StrixMusic.Sdk.Tests.Plugins.PopulateEmptyNames
                 case "default":
                     plugin.EmptyDefaultName = string.Empty;
                     Assert.AreEqual(0, plugin.Playable.Count);
+                    break;
+                default:
+                    Assert.Fail();
+                    break;
+            }
+        }
+
+        [DataRow("album")]
+        [DataRow("artist")]
+        [DataRow("playlist")]
+        [DataRow("track")]
+        [TestMethod]
+        public void GlobalPluginConnectorAppliesDefaultTo(string target)
+        {
+            var plugin = CreatePlugin();
+
+            Assert.AreNotEqual(string.Empty, plugin.EmptyAlbumName);
+            Assert.AreNotEqual(string.Empty, plugin.EmptyArtistName);
+            Assert.AreNotEqual(string.Empty, plugin.EmptyTrackName);
+            Assert.AreNotEqual(string.Empty, plugin.EmptyPlaylistName);
+            Assert.AreNotEqual(string.Empty, plugin.EmptyDefaultName);
+
+            var core = new MockCore();
+            var album = (MergedAlbum)MockItemFactory.CreateAlbum(core);
+            var artist = (MergedArtist)MockItemFactory.CreateArtist(core);
+            var playlist = (MergedPlaylist)MockItemFactory.CreatePlaylist(core);
+            var track = (MergedTrack)MockItemFactory.CreateTrack(core);
+
+            album.Name = string.Empty;
+            artist.Name = string.Empty;
+            playlist.Name = string.Empty;
+            track.Name = string.Empty;
+            
+            // Clear all plugins that should be covered by default
+            plugin.EmptyAlbumName = string.Empty;
+            plugin.EmptyArtistName = string.Empty;
+            plugin.EmptyPlaylistName = string.Empty;
+            plugin.EmptyTrackName = string.Empty;
+            plugin.Album.Clear();
+            plugin.Artist.Clear();
+            plugin.Playlist.Clear();
+            plugin.Track.Clear();
+
+            Assert.AreEqual(0, plugin.Album.Count);
+            Assert.AreEqual(0, plugin.Artist.Count);
+            Assert.AreEqual(0, plugin.Playlist.Count);
+            Assert.AreEqual(0, plugin.Track.Count);
+            Assert.AreEqual(1, plugin.Playable.Count, IncorrectPluginCountMsg);
+
+            var connectedPlugin = GlobalModelPluginConnector.Create(plugin);
+            
+            switch (target)
+            {
+                case "album":
+                    Assert.AreNotEqual(album.Name, connectedPlugin.Album.Execute(album).Name);
+                    break;
+                case "artist":
+                    Assert.AreNotEqual(artist.Name, connectedPlugin.Artist.Execute(artist).Name);
+                    break;
+                case "playlist":
+                    Assert.AreNotEqual(playlist.Name, connectedPlugin.Playlist.Execute(playlist).Name);
+                    break;
+                case "track":
+                    Assert.AreNotEqual(track.Name, connectedPlugin.Track.Execute(track).Name);
                     break;
                 default:
                     Assert.Fail();
