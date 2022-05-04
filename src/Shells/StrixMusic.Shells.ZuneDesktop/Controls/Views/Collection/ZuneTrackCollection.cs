9607f2c9e23dc5cf8651de87267de112d9f87039
amaidniazi <amaidhassan@hotmail.com>
2022-05-04 13:23:31 +0100

    WIP - track collection artist column.

---
+++ src/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/ZuneTrackCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StrixMusic.Sdk.WinUI.Controls.Collections;
+
+namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collection
+{
+    /// <summary>
+    /// Zune implmenation for the <see cref="ZuneTrackCollection"/>.
+    /// </summary>
+    public class ZuneTrackCollection : TrackCollection
+    {
+        /// <summary>
+        /// Creates a new instace for <see cref="ZuneTrackCollection"/>.
+        /// </summary>
+        public ZuneTrackCollection()
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+    }
+}
---
+++ src/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Items/ZuneTrackItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Items
+{
+    /// <summary>
+    /// Zune implemenation for <see cref="ZuneTrackItem"/>.
+    /// </summary>
+    public class ZuneTrackItem
+    {
+    }
+}
