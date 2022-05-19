5bf9a6c5a6d2fa3b5df3bb38049a4d5fd876b9b1
Arlo <arlo.godfrey@outlook.com>
2022-05-19 00:47:05 -0500

    Added git repo and build dependencies to release content

--- build-scripts/OrganizeReleaseContent.ps1
+++ build-scripts/OrganizeReleaseContent.ps1
@@ -8,12 +8,20 @@ Param (
     [Parameter(HelpMessage = "The path to a directory containing the compiled documentation website.", Mandatory = $true)]
     [string]$docsPath,
 
+    [Parameter(HelpMessage = "The path to a directory containing the git repo, without any artifacts.", Mandatory = $true)]
+    [string]$cleanRepoPath,
+
+    [Parameter(HelpMessage = "The path to a directory containing all snapshotted build dependencies", Mandatory = $true)]
+    [string]$buildDependenciesPath,
+
     [Parameter(HelpMessage = "The path to the directory where the release content is placed.", Mandatory = $true)]
     [string]$outputPath
 )
 
 $docsdest = "$outputPath/docs"
 $wasmdest = "$outputPath/app"
+$gitdest = "$outputPath/git"
+$builddepdest = "$outputPath/dependencies"
 $websitedest = $outputPath
 
 Write-Host "Creating folder $outputPath"
@@ -25,6 +33,12 @@ mkdir $docsdest
 Write-Host "Creating folder $wasmdest"
 mkdir $wasmdest
 
+Write-Host "Creating folder $gitdest"
+mkdir $gitdest
+
+Write-Host "Creating folder $builddepdest"
+mkdir $builddepdest
+
 Write-Host "Copying contents from $websitePath to $websitedest"
 Copy-Item -PassThru -Recurse -Path $websitePath -Destination $websitedest
 
@@ -33,3 +47,9 @@ Copy-Item -PassThru -Recurse -Path $wasmAppPath -Destination $docsdest
 
 Write-Host "Copying contents from $wasmsrc to $wasmdest"
 Copy-Item -PassThru -Recurse -Path $wasmAppPath -Destination $wasmdest
+
+Write-Host "Copying contents from $cleanRepoPath/* to $gitdest"
+Copy-Item -PassThru -Recurse -Path $cleanRepoPath/* -Destination $gitdest
+
+Write-Host "Copying contents from $buildDependenciesPath to $builddepdest"
+Copy-Item -PassThru -Recurse -Path $buildDependenciesPath -Destination $builddepdest
