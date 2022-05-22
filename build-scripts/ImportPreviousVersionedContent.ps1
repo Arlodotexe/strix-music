fdbc7e4f200e9b0f4ec31fbc9fdf88c89d6a8966
Arlo <arlo.godfrey@outlook.com>
2022-05-21 21:43:43 -0500

    Added previous version tracking with script and versions.json

--- build-scripts/ImportPreviousVersionedContent.ps1
+++ build-scripts/ImportPreviousVersionedContent.ps1
@@ -39,5 +39,36 @@ if ($ipfsCid.ToLower().Contains("error")) {
     exit -1;
 }
 
+$versionsJsonPath = "$outputPath/versions.json";
+Write-Output "Importing content"
+
+# Strix Music SDK Nuget packages
 ipfs get --output $outputPath/sdk/nupkg/ $ipfsCid/sdk/nupkg/
-ipfs get --output $outputPath/app/windows/ $ipfsCid/app/windows/
+
+# List of previous versions
+ipfs get --output $outputPath/versions.json $ipfsCid/versions.json
+
+Write-Output "Updating versions.json";
+if (Test-Path $versionsJsonPath) {
+    $versions = Get-Content -Path $versionsJsonPath -Force | ConvertFrom-Json;
+} else {
+    New-Item -ItemType File -Path $versionsJsonPath -ErrorAction Stop | Out-Null;
+    $versions = ("[]" | ConvertFrom-Json);
+}
+
+if ($versions -isnot [array]) {
+    $versions = @($versions);
+}
+
+if (($versions | Where-Object {$_.cid -eq $ipfsCid})) {
+    Write-Error "Unable to continue. Version already published."
+    exit -1;
+}
+
+$versions += [PSCustomObject]@{
+    cid = $ipfsCid
+    unixTimestamp = ((([System.DateTime]::Now) - ([System.DateTime]::UnixEpoch)).TotalSeconds)
+}
+
+Set-Content -Path $versionsJsonPath -Value ($versions | ConvertTo-Json);
+Write-Output "Archived CID of previous version under $versionsJsonPath"
