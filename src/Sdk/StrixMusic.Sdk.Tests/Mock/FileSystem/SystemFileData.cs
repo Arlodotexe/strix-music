da0016d71fd3a800d40f7ce10efaddc7f603b6b7
Amaid Niazi <amaidniazi@gmail.com>
2022-05-31 21:47:45 +0100

    [fix] Audio Metadata Scanner now returns multiple artists for a track (if any) (#106)
    * Refactored AudioMetadataScanner, FileMetadata and FileMetadataManager to spit multiple artists for a track
    
    * Added unit tests for the case where audio metadata is pulling tracks with more than one artist.
    
    * Removed unused TaglibMockData class.
    
    * Fixed and refactored AudioMetadataScanner unit tests.
    
    * Removed stray event handler and cleanup.
    
    * Update src/Sdk/StrixMusic.Sdk.Tests/Services/MetadataScanner/AudioMetadataScanner.Tests.cs
    
    Co-authored-by: amaidniazi <amaidhassan@hotmail.com>
    Co-authored-by: Arlo <arlo.godfrey@outlook.com>

---
+++ src/Sdk/StrixMusic.Sdk.Tests/Mock/FileSystem/SystemFileData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OwlCore.AbstractStorage;
+
+namespace StrixMusic.Sdk.Tests.Mock.FileSystem
+{
+    internal class SystemFileData : IFileData
+    {
+        private string _path;
+
+        public SystemFileData(string path)
+        {
+            _path = path;
+        }
+
+        public string? Id { get; }
+
+        public string Path => _path;
+
+        public string Name => System.IO.Path.GetFileName(_path);
+
+        public string DisplayName => System.IO.Path.GetFileName(_path);
+
+        public string FileExtension => System.IO.Path.GetExtension(_path);
+
+        public IFileDataProperties Properties { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task Delete() => throw new NotImplementedException();
+        public Task<IFolderData> GetParentAsync() => throw new NotImplementedException();
+        public Task<Stream> GetStreamAsync(FileAccessMode accessMode = FileAccessMode.Read)
+        {
+            using var fs = File.Open(_path, FileMode.Open, FileAccess.ReadWrite);
+
+            fs.Position = 0;
+            var stream = new MemoryStream();
+            fs.CopyTo(stream);
+
+            return Task.FromResult((Stream)stream);
+        }
+        public Task<Stream> GetThumbnailAsync(ThumbnailMode thumbnailMode, uint requiredSize) => throw new NotImplementedException();
+        public async Task WriteAllBytesAsync(byte[] bytes) => await File.WriteAllBytesAsync(_path, bytes);
+    }
+}
