787769781fa9d819870e6119285f2cba5403422c
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-06-15 23:15:59 -0500

    Removed INotificationService dependency in FileMetadataManager. File based cores now report scan progress via the Progress{T} in the constructor.

---
+++ src/Sdk/StrixMusic.Sdk/FileMetadata/FileScanState.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Arlo Godfrey. All Rights Reserved.
+// Licensed under the GNU Lesser General Public License, Version 3.0 with additional terms.
+// See the LICENSE, LICENSE.LESSER and LICENSE.ADDITIONAL files in the project root for more information.
+
+namespace StrixMusic.Sdk.FileMetadata
+{
+    /// <summary>
+    /// Indicates the overall state of an ongoing file scan.
+    /// </summary>
+    public struct FileScanState
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileScanState"/> struct.
+        /// </summary>
+        public FileScanState(FileScanStage stage, int filesProcessed, int filesFound)
+            : this()
+        {
+            Stage = stage;
+            FilesProcessed = filesProcessed;
+            FilesFound = filesFound;
+        }
+        
+        /// <summary>
+        /// The total number of files discovered that need to be processed.
+        /// </summary>
+        public int FilesFound { get; }
+
+        /// <summary>
+        /// The number of files that have been processed.
+        /// </summary>
+        public int FilesProcessed { get; }
+
+        /// <summary>
+        /// The current stage of the file scanner.
+        /// </summary>
+        public FileScanStage Stage { get; }
+    }
+}
