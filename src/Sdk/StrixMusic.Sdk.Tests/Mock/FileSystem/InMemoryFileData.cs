41e81f9c54d88133cd1e4855c4903af9d227df48
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-06-15 21:16:09 -0500

    Added TrackRepository unit tests for track order

---
+++ src/Sdk/StrixMusic.Sdk.Tests/Mock/FileSystem/InMemoryFileData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using OwlCore.AbstractStorage;
+
+namespace StrixMusic.Sdk.Tests.Mock.FileSystem
+{
+    public class InMemoryFileData : IFileData
+    {
+        public Stream _stream = new MemoryStream();
+
+        public InMemoryFileData(string path, string name, string fileExtension)
+        {
+            Path = path;
+            Name = name;
+            DisplayName = name;
+            FileExtension = fileExtension;
+        }
+
+        public string? Id { get; set; }
+
+        public string Path { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public IFileDataProperties Properties { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task Delete() => _stream.DisposeAsync().AsTask();
+
+        public Task<IFolderData> GetParentAsync() => throw new NotSupportedException();
+
+        public Task<Stream> GetStreamAsync(FileAccessMode accessMode = FileAccessMode.Read) => Task.FromResult(_stream);
+
+        public Task<Stream> GetThumbnailAsync(ThumbnailMode thumbnailMode, uint requiredSize) => throw new NotImplementedException();
+
+        public Task WriteAllBytesAsync(byte[] bytes) => _stream.WriteAsync(bytes).AsTask();
+    }
+}
