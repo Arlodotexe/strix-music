477f532d0f06081c69fd0dcf6f5b83223031bed1
Arlo <arlo.godfrey@outlook.com>
2022-07-25 12:02:34 -0500

    [fix] Fixed WASM app not loading due to missing SharedArrayBuffer (#204)
    Fixed WASM app not loading due to missing SharedArrayBuffer

---
+++ src/Platforms/StrixMusic.Wasm/wwwroot/scripts/EnableSharedArrayBuffer.js
@@ -0,0 +1,19 @@
+﻿// From https://dev.to/stefnotch/enabling-coop-coep-without-touching-the-server-2d3n
+
+if ("serviceWorker" in navigator) {
+    // Register service worker
+    navigator.serviceWorker.register(new URL("../SharedArrayBufferServiceWorker.js", window.location.href)).then(
+        function (registration) {
+            console.log("COOP/COEP Service Worker registered", registration.scope);
+            // If the registration is active, but it's not controlling the page
+            if (registration.active && !navigator.serviceWorker.controller) {
+                window.location.reload();
+            }
+        },
+        function (err) {
+            console.log("COOP/COEP Service Worker failed to register", err);
+        }
+    );
+} else {
+    console.warn("Cannot register a service worker");
+}
