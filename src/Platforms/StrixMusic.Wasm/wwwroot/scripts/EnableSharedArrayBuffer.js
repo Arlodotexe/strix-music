e8f08a911c5b54da4ecf1296bf834dd4e10d151d
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-07-28 19:24:51 -0500

    Fixed SharedArrayBuffer workaround when not hosted at domain root

--- src/Platforms/StrixMusic.Wasm/wwwroot/scripts/EnableSharedArrayBuffer.js
+++ src/Platforms/StrixMusic.Wasm/wwwroot/scripts/EnableSharedArrayBuffer.js
@@ -2,7 +2,7 @@
 
 if ("serviceWorker" in navigator) {
     // Register service worker
-    navigator.serviceWorker.register(new URL("../SharedArrayBufferServiceWorker.js", window.location.href)).then(
+    navigator.serviceWorker.register(new URL("SharedArrayBufferServiceWorker.js", window.location.href)).then(
         function (registration) {
             console.log("COOP/COEP Service Worker registered", registration.scope);
             // If the registration is active, but it's not controlling the page
