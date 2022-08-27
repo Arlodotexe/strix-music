b46a8ffd48e9e367733a6900598f3b6a8fffd714
amaidniazi <amaidhassan@hotmail.com>
2022-08-27 15:24:32 +0100

    Code cleanup.

--- src/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/ZuneTrackCollectionItem.cs
+++ src/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/ZuneTrackCollectionItem.cs
@@ -19,22 +19,30 @@ namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collection
     /// </summary>
     public sealed partial class ZuneTrackCollectionItem : ObservableObject
     {
-        // Cache artist item values
-        // Keeping a minimum cache that updates with events allows us to
-        // avoid checking all items when a single item updates.
-        private ConcurrentDictionary<string, int> _lastKnownTrackArtistsCount = new();
-
         [ObservableProperty]
         private ITrackCollectionViewModel? _parentCollection;
 
         [ObservableProperty]
         private TrackViewModel? _track;
 
-        [ObservableProperty]
         private bool _shouldShowArtistList;
 
         [ObservableProperty]
         private ObservableCollection<MetadataItem> _artistNamesMetadata = new ObservableCollection<MetadataItem>();
+
+        /// <summary>
+        /// Flag to determine whether to show artist column or not.
+        /// </summary>
+        public bool ShouldShowArtistList
+        {
+            get => _shouldShowArtistList;
+
+            set
+            {
+                _shouldShowArtistList = value;
+                OnPropertyChanged(nameof(ShouldShowArtistList));
+            }
+        }
 
         partial void OnTrackChanging(TrackViewModel? newValue)
         {
@@ -64,11 +72,6 @@ namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collection
 
             async Task ExecuteAsync(ITrackCollectionViewModel? newValue)
             {
-                if (ParentCollection is not null)
-                {
-                    DetachEvents(ParentCollection);
-                }
-
                 if (newValue is null)
                     return;
 
@@ -77,43 +80,6 @@ namespace StrixMusic.Shells.ZuneDesktop.Controls.Views.Collection
                 // If any track in the parent collection has more than 2 artists,
                 // ALL track items should display their artist list, including this instance.
                 ShouldShowArtistList = newValue.Tracks.Any(x => x.TotalArtistItemsCount > 1) && newValue is IAlbum or IArtist;
-
-                lock (_lastKnownTrackArtistsCount)
-                {
-                    _lastKnownTrackArtistsCount.Clear();
-
-                    foreach (var item in newValue.Tracks)
-                    {
-                        _lastKnownTrackArtistsCount[item.Id] = item.TotalArtistItemsCount;
-                    }
-
-                    AttachEvents(newValue);
-                }
-            }
-        }
-
-        private void AttachEvents(ITrackCollectionViewModel trackCollection)
-        {
-            trackCollection.TracksChanged += TrackCollection_TracksChanged;
-        }
-
-        private void DetachEvents(ITrackCollectionViewModel trackCollection)
-        {
-            trackCollection.TracksChanged -= TrackCollection_TracksChanged;
-        }
-
-        private void TrackCollection_TracksChanged(object sender, IReadOnlyList<OwlCore.Events.CollectionChangedItem<ITrack>> addedItems, IReadOnlyList<OwlCore.Events.CollectionChangedItem<ITrack>> removedItems)
-        {
-            // Handle newly added/removed tracks
-            lock (_lastKnownTrackArtistsCount)
-            {
-                foreach (var item in removedItems)
-                    Guard.IsTrue(_lastKnownTrackArtistsCount.TryRemove(item.Data.Id, out _));
-
-                foreach (var item in addedItems)
-                    Guard.IsTrue(_lastKnownTrackArtistsCount.TryAdd(item.Data.Id, item.Data.TotalArtistItemsCount));
-
-                ShouldShowArtistList = _lastKnownTrackArtistsCount.Any(x => x.Value > 1) && _parentCollection is IAlbum or IArtist;
             }
         }
     }
