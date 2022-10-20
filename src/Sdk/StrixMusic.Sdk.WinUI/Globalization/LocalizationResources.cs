35d5dc2fe9e296f2fa2811ea0c3aff56f36a5ada
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-10-20 14:33:30 -0500

    Fixed missing assembly name for localization

--- src/Sdk/StrixMusic.Sdk.WinUI/Globalization/LocalizationResources.cs
+++ src/Sdk/StrixMusic.Sdk.WinUI/Globalization/LocalizationResources.cs
@@ -10,16 +10,16 @@ namespace StrixMusic.Sdk.WinUI.Globalization
         /// <summary>
         /// The resource loader for common strings.
         /// </summary>
-        public static ResourceLoader? Common => ResourceLoader.GetForCurrentView(nameof(Common));
+        public static ResourceLoader? Common => ResourceLoader.GetForCurrentView($"StrixMusic.Sdk.WinUI/{nameof(Common)}");
 
         /// <summary>
         /// The resource loader for time-related strings.
         /// </summary>
-        public static ResourceLoader? Time => ResourceLoader.GetForCurrentView(nameof(Time));
+        public static ResourceLoader? Time => ResourceLoader.GetForCurrentView($"StrixMusic.Sdk.WinUI/{nameof(Time)}");
 
         /// <summary>
         /// The resource loader for generic music-related strings.
         /// </summary>
-        public static ResourceLoader? Music => ResourceLoader.GetForCurrentView(nameof(Music));
+        public static ResourceLoader? Music => ResourceLoader.GetForCurrentView($"StrixMusic.Sdk.WinUI/{nameof(Music)}");
     }
 }
