1dc80acd0ce3043a773621e20661c205f223f9fa
amaidniazi <amaidhassan@hotmail.com>
2022-12-29 10:52:00 +0000

    Removed stray comments on usercontrol.

--- src/Shared/Controls/DebugLogs.xaml.cs
+++ src/Shared/Controls/DebugLogs.xaml.cs
@@ -3,8 +3,6 @@ using StrixMusic.AppModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace StrixMusic.Controls
 {
     /// <summary>
