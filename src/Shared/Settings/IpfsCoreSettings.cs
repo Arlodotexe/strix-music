54bc16bee1ae5d01dc4f11c325ebfcc5e2da0ee8
amaidniazi <amaidhassan@hotmail.com>
2023-03-31 22:30:41 +0100

    Fixed the ipfs core settings.

--- src/Shared/Settings/IpfsCoreSettings.cs
+++ src/Shared/Settings/IpfsCoreSettings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using ColorCode.Common;
 using OwlCore.ComponentModel;
 using OwlCore.Kubo;
 using OwlCore.Storage;
@@ -43,7 +44,7 @@ namespace StrixMusic.Settings
         /// <summary>
         /// Gets or sets an ID 
         /// </summary>
-        public string? IpfsCidPath
+        public string IpfsCidPath
         {
             get => GetSetting(() => string.Empty);
             set => SetSetting(value);
@@ -52,19 +53,32 @@ namespace StrixMusic.Settings
         /// <summary>
         /// Gets or sets an ID 
         /// </summary>
-        public string? IpnsAddress
+        public string IpnsAddress
         {
             get => GetSetting(() => string.Empty);
             set => SetSetting(value);
         }
 
         /// <inheritdoc />
-        public override object GetSettingByName(string settingName) => throw new NotImplementedException();
+        public override bool IsSettingValidForCoreCreation(string propertyName, object? value) => propertyName switch
+        {
+            nameof(InstanceId) or nameof(InstanceId) or nameof(IpfsCidPath) or nameof(IpfsCidPath)
+                => !string.IsNullOrWhiteSpace((string?)value ?? string.Empty),
+            _ => true,
+        };
 
         /// <inheritdoc />
-        public override bool IsSettingValidForCoreCreation(string propertyName, object? value)
+        public override object GetSettingByName(string settingName)
         {
-            return !string.IsNullOrWhiteSpace((string?)value ?? string.Empty);
+            Guard.ArgNotNull(settingName, nameof(settingName));
+
+            return settingName switch
+            {
+                nameof(InstanceId) => InstanceId,
+                nameof(IpfsCidPath) => IpfsCidPath,
+                nameof(IpnsAddress) => IpnsAddress,
+                _ => throw new ArgumentOutOfRangeException(nameof(settingName), settingName, @"Unknown setting name specified.")
+            };
         }
     }
 }
