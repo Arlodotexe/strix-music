cd927d634682e468bd4f7b5d0ac421f99fb60df5
Arlo Godfrey <arlo.godfrey@outlook.com>
2023-04-25 23:31:21 -0500

    Release 0.1.0-sdk-alpha, Release 0.1.0-app-alpha

--- build-scripts/GenerateChangelogs.ps1
+++ build-scripts/GenerateChangelogs.ps1
@@ -189,7 +189,7 @@ if ((GetGroupByName "new").length -gt 0) {
 }
 
 if ((GetGroupByName "refactor").length -gt 0 -or (GetGroupByName "cleanup").length -gt 0) {
-    $changelogMarkdownLines += "`n## Maintainence"
+    $changelogMarkdownLines += "`n## Maintenance"
     foreach ($commit in GetGroupByName "refactor") {
         $changelogMarkdownLines += TransformCommitToMarkdown $commit
     }
