d2829a98af609db97ae925e7c36c8a01a2a9d78a
Arlo Godfrey <arlo.godfrey@outlook.com>
2023-04-25 21:28:25 -0500

    Set up debug OneDrive secrets partial

--- src/Shared/Secrets.cs
+++ src/Shared/Secrets.cs
@@ -1,29 +1,30 @@
-﻿namespace StrixMusic
+﻿using StrixMusic.Settings;
+
+namespace StrixMusic;
+
+/// <summary>
+/// Holds important data needed to initialize the app in a production environment.
+/// </summary>
+/// <remarks> Preconfigured for development. Add another partial and use a "!DEBUG" compilation conditional to configure for release mode.
+/// <para /> IMPORTANT:
+/// <para /> You will be prompted in-app for the missing data, do not modify this file.
+/// </remarks>
+internal static partial class Secrets
 {
-    /// <summary>
-    /// Holds important data needed to initialize the app in a production environment.
-    /// </summary>
-    /// <remarks> Preconfigured for development. Add another partial and use a "!DEBUG" compilation conditional to configure for release mode.
-    /// <para /> IMPORTANT:
-    /// <para /> You will be prompted in-app for the missing data, do not modify this file.
-    /// </remarks>
-    internal static partial class Secrets
-    {
 #if DEBUG
-        /// <summary>
-        /// The <see cref="OneDriveCoreSettings.ClientId"/> for <see cref="OneDriveCore"/>.
-        /// </summary>
-        public static string OneDriveClientId { get; } = string.Empty;
+    /// <summary>
+    /// The <see cref="OneDriveCoreSettings.ClientId"/>.
+    /// </summary>
+    public static string OneDriveClientId { get; } = string.Empty;
 
-        /// <summary>
-        /// The <see cref="OneDriveCoreSettings.TenantId"/> for <see cref="OneDriveCore"/>.
-        /// </summary>
-        public static string OneDriveTenantId { get; } = string.Empty;
+    /// <summary>
+    /// The <see cref="OneDriveCoreSettings.TenantId"/>.
+    /// </summary>
+    public static string OneDriveTenantId { get; } = string.Empty;
 
-        /// <summary>
-        /// The <see cref="OneDriveCoreSettings.RedirectUri"/> for <see cref="OneDriveCore"/>.
-        /// </summary>
-        public static string OneDriveRedirectUri { get; } = string.Empty;
+    /// <summary>
+    /// The <see cref="OneDriveCoreSettings.RedirectUri"/>.
+    /// </summary>
+    public static string OneDriveRedirectUri { get; } = string.Empty;
 #endif
-    }
 }
