c816b0938cb6d05cc174d5906ddac59dcd29608b
Adam Dernis <Avid29@Live.com>
2025-09-28 11:00:05 +0300

    Updated all packages to modern sensibilities

--- src/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/ZuneTrackCollectionItem.cs
+++ src/Shells/StrixMusic.Shells.ZuneDesktop/Controls/Views/Collection/ZuneTrackCollectionItem.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Uwp.UI.Controls;
+using CommunityToolkit.WinUI.Controls;
 using StrixMusic.Sdk.AppModels;
 using StrixMusic.Sdk.ViewModels;
 
